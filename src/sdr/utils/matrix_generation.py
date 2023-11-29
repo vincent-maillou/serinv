2ee46944ad8b27c3ac73db019908d90d44f0298c
vincent-maillou <vmaillou@iis.ee.ethz.ch>
2023-11-29 10:18:34 +0100

    Added routine sot create ndiags arrowhead matrices

--- src/sdr/utils/matrix_generation.py
+++ src/sdr/utils/matrix_generation.py
@@ -183,6 +183,76 @@ def generate_blocktridiag_arrowhead(
     return A
 
 
+
+def generate_ndiags_arrowhead(
+    nblocks: int,
+    ndiags: int,
+    diag_blocksize: int,    
+    arrow_blocksize: int,    
+    symmetric: bool = False,
+    diagonal_dominant: bool = False,  
+    seed: int = None,
+) -> np.ndarray:
+    """ Generate a block tridiagonal arrowhead matrix.
+
+    Parameters
+    ----------
+    nblocks : int
+        Number of diagonal blocks.
+    ndiags : int
+        Number of diagonals.
+    diag_blocksize : int
+        Size of the diagonal blocks.
+    arrow_blocksize : int
+        Size of the arrowhead blocks. These blocks will be of sizes: 
+        (arrow_blocksize*diag_blocksize).
+    symmetric : bool, optional
+        If True, the matrix will be symmetric.
+    seed : int, optional
+        Seed for the random number generator.
+
+    Returns
+    -------
+    A : np.ndarray
+        Block tridiagonal arrowhead matrix.
+    """
+
+    if seed is not None:
+        np.random.seed(seed)
+
+    matrice_size = (nblocks-1)*diag_blocksize+arrow_blocksize
+
+    A = np.zeros((matrice_size, matrice_size))
+
+    for i in range(nblocks):
+        if i < nblocks-1:
+            A[i*diag_blocksize:(i+1)*diag_blocksize, i*diag_blocksize:(i+1)*diag_blocksize] = np.random.rand(diag_blocksize, diag_blocksize)
+            for j in range(1, (ndiags+1)//2):
+                if i+j < nblocks-1:
+                    A[i*diag_blocksize:(i+1)*diag_blocksize, (i+j)*diag_blocksize:(i+j+1)*diag_blocksize] = np.random.rand(diag_blocksize, diag_blocksize)
+                    A[(i+j)*diag_blocksize:(i+j+1)*diag_blocksize, i*diag_blocksize:(i+1)*diag_blocksize] = np.random.rand(diag_blocksize, diag_blocksize)
+                if i-j >= 0:
+                    A[i*diag_blocksize:(i+1)*diag_blocksize, (i-j)*diag_blocksize:(i-j+1)*diag_blocksize] = np.random.rand(diag_blocksize, diag_blocksize)
+                    A[(i-j)*diag_blocksize:(i-j+1)*diag_blocksize, i*diag_blocksize:(i+1)*diag_blocksize] = np.random.rand(diag_blocksize, diag_blocksize)
+
+
+            A[(nblocks-1)*diag_blocksize:(nblocks-1)*diag_blocksize+arrow_blocksize, i*diag_blocksize:(i+1)*diag_blocksize] = np.random.rand(arrow_blocksize, diag_blocksize)
+            A[i*diag_blocksize:(i+1)*diag_blocksize, (nblocks-1)*diag_blocksize:(nblocks-1)*diag_blocksize+arrow_blocksize] = np.random.rand(diag_blocksize, arrow_blocksize)
+        
+        else:
+            A[i*diag_blocksize:i*diag_blocksize+arrow_blocksize, i*diag_blocksize:i*diag_blocksize+arrow_blocksize] = np.random.rand(arrow_blocksize, arrow_blocksize)
+    
+
+    if symmetric:
+        A = A + A.T
+
+    if diagonal_dominant:
+        A = make_diagonally_dominante(A)
+        
+    return A
+
+
+
 def make_diagonally_dominante(
     A: np.ndarray,
 ) -> np.ndarray:
--- tests/utils_tests/matrix_generation_tests.py
+++ tests/utils_tests/matrix_generation_tests.py
@@ -64,3 +64,24 @@ if __name__ == "__main__":
     plt.matshow(A)
     plt.title("Block " +  str(ndiags) + "-diagonals matrix \n blocksize = " + str(blocksize))
     plt.show()
+
+
+
+if __name__ == "__main__":
+    nblocks = 7
+    ndiags = 5
+    diag_blocksize = 3
+    arrow_blocksize = 2
+    symmetric = True
+    diagonal_dominant = True
+    seed = 63
+
+    A = matrix_generation.generate_ndiags_arrowhead(
+        nblocks, ndiags, diag_blocksize, arrow_blocksize, symmetric, 
+        diagonal_dominant, seed
+    )
+
+    plt.matshow(A)
+    plt.title("Block " +  str(ndiags) + "-diagonals arrowhead matrix \n diag_blocksize = " + str(diag_blocksize) + "\n arrow_blocksize = " + str(arrow_blocksize))
+    plt.show()
+    
