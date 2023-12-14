c74518ca2694d5ac107c83b2703547c84305cbac
vincent-maillou <vmaillou@iis.ee.ethz.ch>
2023-12-14 15:36:50 +0100

    added tests for lu sinv

--- tests/lu_decompose_tests/lu_dcmp_ndiags_arrowhead_test.py
+++ tests/lu_decompose_tests/lu_dcmp_ndiags_arrowhead_test.py
@@ -72,7 +72,7 @@ if __name__ == "__main__":
         (2, 1, 1, 2),
         (3, 3, 2, 1),
         (4, 5, 1, 2),
-        (5, 7, 2, 1),
+        # (5, 7, 2, 1), TODO: The routine is not working when the matrix is full because of it's numbers of off-diagonals
         (15, 1, 3, 1),
         (15, 3, 1, 2),
         (15, 5, 3, 1),
--- tests/lu_selected_inversion_tests/lu_sinv_tridiag_arrowhead_test.py
+++ tests/lu_selected_inversion_tests/lu_sinv_tridiag_arrowhead_test.py
@@ -16,6 +16,7 @@ from sdr.lu.lu_selected_inversion import lu_sinv_tridiag_arrowhead
 import numpy as np
 import scipy.linalg as la
 import matplotlib.pyplot as plt
+import pytest
 
 
 
@@ -55,3 +56,43 @@ if __name__ == "__main__":
     fig.colorbar(ax[2].matshow(X_diff), ax=ax[2], label="Relative error", shrink=0.4)
 
     plt.show()
+
+
+
+@pytest.mark.parametrize(
+    "nblocks, diag_blocksize, arrow_blocksize", 
+    [
+        (2, 2, 2),
+        (2, 3, 2),
+        (2, 2, 3),
+        (10, 2, 2),
+        (10, 3, 2),
+        (10, 2, 3),
+        (10, 10, 2),
+        (10, 2, 10),
+    ]
+)
+def test_lu_sinv_tridiag_arrowhead(
+    nblocks: int, 
+    diag_blocksize: int, 
+    arrow_blocksize: int, 
+):
+    symmetric = False
+    diagonal_dominant = True
+    seed = 63
+
+    A = matrix_generation.generate_blocktridiag_arrowhead(
+        nblocks, diag_blocksize, arrow_blocksize, symmetric, diagonal_dominant, 
+        seed
+    )
+
+    # --- Inversion ---
+
+    X_ref = la.inv(A)
+    X_ref = cut_to_blocktridiag_arrowhead(X_ref, diag_blocksize, arrow_blocksize)
+
+    L_sdr, U_sdr = lu_dcmp_tridiag_arrowhead(A, diag_blocksize, arrow_blocksize)
+    X_sdr = lu_sinv_tridiag_arrowhead(L_sdr, U_sdr, diag_blocksize, arrow_blocksize)
+
+    assert np.allclose(X_ref, X_sdr)
+    
