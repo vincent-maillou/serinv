f43d75010287915f284adbc768c52e9baf034381
vincent-maillou <vmaillou@iis.ee.ethz.ch>
2024-02-24 12:11:10 +0100

    Added break-even plot for computation cost

--- src/sdr/psr/theoretical_analysis.py
+++ src/sdr/psr/theoretical_analysis.py
@@ -155,7 +155,7 @@ if __name__ == "__main__":
     diag_blocksize = 4000
     arrowhead_blocksize = 4
 
-    max_n_blocks = 10000
+    max_n_blocks = 3000
     max_partitions = 128
 
     cost_matrix = np.zeros((max_partitions, max_n_blocks))
@@ -173,11 +173,27 @@ if __name__ == "__main__":
             n_blocks_partition = get_partition_size(n_blocks, n_partitions)
             reduced_system_size = get_reduced_system_size(n_partitions)
 
+            # Standard algorithm analysis
             cost_bta_dist_middle_process_factorization = getcost_bta_middle_dist_factorization(n_blocks_partition, diag_blocksize, arrowhead_blocksize)
             cost_bta_dist_reduced_system_solve = getcost_bta_seq_factorization(reduced_system_size, diag_blocksize, arrowhead_blocksize) + getcost_bta_seq_sinv(reduced_system_size, diag_blocksize, arrowhead_blocksize)
             cost_bta_dist_middle_process_sinv = getcost_bta_middle_dist_sinv(n_blocks_partition, diag_blocksize, arrowhead_blocksize)
-
             cost_bta_dist = sp.simplify(cost_bta_dist_middle_process_factorization + cost_bta_dist_reduced_system_solve + cost_bta_dist_middle_process_sinv)
+
+
+            """ # Divid and conquer algorithm aproach for the reduced system inversion
+            cost_bta_dist_middle_process_factorization = getcost_bta_middle_dist_factorization(n_blocks_partition, diag_blocksize, arrowhead_blocksize)
+            
+            blocks_per_process = 3
+            n_processes_inverting_reduced_system = reduced_system_size // blocks_per_process
+            reduced_system_sub_partition_size = get_reduced_system_size(n_processes_inverting_reduced_system)
+            cost_bta_dist_reduced_system_solve = getcost_bta_middle_dist_factorization(blocks_per_process, diag_blocksize, arrowhead_blocksize) + getcost_bta_middle_dist_sinv(blocks_per_process, diag_blocksize, arrowhead_blocksize)
+            cost_bta_dist_reduced_system_solve += getcost_bta_seq_factorization(reduced_system_sub_partition_size, diag_blocksize, arrowhead_blocksize) + getcost_bta_seq_sinv(reduced_system_sub_partition_size, diag_blocksize, arrowhead_blocksize)
+            
+            cost_bta_dist_middle_process_sinv = getcost_bta_middle_dist_sinv(n_blocks_partition, diag_blocksize, arrowhead_blocksize)
+            
+            cost_bta_dist = sp.simplify(cost_bta_dist_middle_process_factorization + cost_bta_dist_reduced_system_solve + cost_bta_dist_middle_process_sinv) """
+
+
 
             cost_matrix[n_partitions, n_blocks] = cost_bta_seq / cost_bta_dist
 
@@ -185,20 +201,52 @@ if __name__ == "__main__":
     print(f"Bench up to: {max_n_blocks} blocks and {max_partitions} partitions")
     print(f"diagonal_blocksize {diag_blocksize}, arrowhead_blocksize {arrowhead_blocksize}")
 
-    n_processes = [1, 2, 4, 8, 16, 32, 64, 128]
-    for p in n_processes:
-        max_ratio = np.max(cost_matrix[p-1, :])
-        max_ratio_nblocks = np.argmax(cost_matrix[p-1, :])
 
-        print(f"Max ratio for {p} processes: {max_ratio} occurs for {max_ratio_nblocks} blocks")
 
-    plt.imshow(cost_matrix, cmap='hot', interpolation='nearest')
-    plt.xlabel("Number of Blocks")
-    plt.ylabel("Number of Partitions")
-
-    # Create a colorbar and put it horizontaly at the bottom
-    plt.colorbar()
-    plt.suptitle("BTA Sequential vs BTA Distributed")
-    plt.title(f"Diagonal block size: {diag_blocksize}, Arrowhead block size: {arrowhead_blocksize}")
-
+    # Extract the trend of the break-even point
+    break_even_points = [1.0, 1.5, 2.0, 2.5, 3.0, 3.5, 4.0, 4.5, 5.0]
+    
+    optimal_n_blocks = np.ndarray((len(break_even_points), max_partitions))
+    
+    for i, break_even_point in enumerate(break_even_points):
+        for p in range(max_partitions):
+            optimal_n_blocks[i, p] = np.argmax(cost_matrix[p, :] >= break_even_point)
+        
+    # Plot the break-even points, excluding zero values
+    for i, break_even_point in enumerate(break_even_points):
+        non_zero_indices = np.where(optimal_n_blocks[i] != 0)[0]  # Find indices of non-zero values
+        plt.plot(non_zero_indices, optimal_n_blocks[i, non_zero_indices], label=f"break-even ratio >= {break_even_point}")
+        
+    plt.title("Number of blocks from which each processes of BTA-dist are doing less than the break-even ration work then BTA-seq")
+    plt.xlabel("Number of processes")
+    plt.ylabel("Number of Blocks")
+    plt.legend()
     plt.show()
+    
+
+    
+    # Create the figure and axes object
+    fig, ax = plt.subplots()
+
+    # Display the image
+    im = ax.imshow(cost_matrix, cmap='inferno', interpolation='nearest')
+
+
+    # Set labels and title
+    ax.set_title(f"Ratio of the computational cost of BTA-seq over BTA-dist \n diag_blocksize = {diag_blocksize} \n arrowhead_blocksize = {arrowhead_blocksize}")
+    
+    ax.set_xlabel("Number of Blocks")
+    ax.set_ylabel("Number of processes")
+    
+    # Add colorbar
+    fig.colorbar(im)
+    #fig.colorbar(im, label='Cost Ratio', ticks=[0.5, 1.0, 1.5], format='%0.2f')
+
+    # Optional: Customize ticks (uncomment if needed)
+    # ax.set_xticks(np.arange(0, max_n_blocks, 250))
+    # ax.set_yticks(np.arange(0, max_partitions, 16))
+
+    # Show the plot
+    plt.show()
+
+
