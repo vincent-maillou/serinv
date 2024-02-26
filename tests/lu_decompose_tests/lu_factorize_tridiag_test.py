a94506ef13ff89a341a2d3983207f4507117a397
vincent-maillou <vmaillou@iis.ee.ethz.ch>
2024-02-26 14:46:06 +0100

    added test for lu factorize

---
+++ tests/lu_decompose_tests/lu_factorize_tridiag_test.py
@@ -0,0 +1,157 @@
+"""
+@author: Vincent Maillou (vmaillou@iis.ee.ethz.ch)
+@author: Lisa Gaedke-Merzhaeuser  (lisa.gaedke.merzhaeuser@usi.ch)
+@date: 2023-11
+
+Tests for lu selected decompositions routines.
+
+Copyright 2023 ETH Zurich and USI. All rights reserved.
+"""
+
+from sdr.utils import matrix_generation
+from sdr.utils.matrix_transform import from_dense_to_tridiagonal_arrays, from_tridiagonal_arrays_to_dense
+from sdr.lu.lu_decompose import lu_factorize_tridiag
+
+import numpy as np
+import scipy.linalg as la
+import matplotlib.pyplot as plt
+import pytest
+
+
+
+# Testing of block tridiagonal lu
+if __name__ == "__main__":
+    nblocks = 5
+    blocksize = 2
+    symmetric = False
+    diagonal_dominant = True
+    seed = 63
+
+    A = matrix_generation.generate_tridiag_dense(
+        nblocks, blocksize, symmetric, diagonal_dominant, seed
+    )
+
+    # --- Decomposition ---
+    # permute_l is default but to raise awareness that this would fail otherwise ...
+    P_ref, L_ref, U_ref = la.lu(A, permute_l=False)
+
+    if not np.allclose(P_ref, np.eye(A.shape[0])):
+        plt.matshow(P_ref)
+        plt.title("WARNING: Permutation matrix should be identity")
+    
+    (
+        A_diagonal_blocks, 
+        A_lower_diagonal_blocks,
+        A_upper_diagonal_blocks, 
+    ) = from_dense_to_tridiagonal_arrays(A, blocksize)
+
+    (
+        L_sdr, 
+        U_sdr
+    ) = lu_factorize_tridiag(
+        A_diagonal_blocks, 
+        A_lower_diagonal_blocks,
+        A_upper_diagonal_blocks, 
+    )
+    
+    
+    L_sdr_dense = from_tridiagonal_arrays_to_dense(
+        L_sdr[:blocksize, :], 
+        np.zeros((blocksize, (nblocks-1)*blocksize)),
+        L_sdr[blocksize:, blocksize:]
+    )
+    
+    U_sdr_dense = from_tridiagonal_arrays_to_dense(
+        U_sdr[blocksize:, :], 
+        U_sdr[:blocksize, :-blocksize],
+        np.zeros((blocksize, (nblocks-1)*blocksize)),
+    )
+    
+    fig, ax = plt.subplots(2, 3)
+    ax[0, 0].set_title("L_ref: Scipy lower factor")
+    ax[0, 0].matshow(L_ref)
+    ax[1, 0].set_title("U_ref: Scipy upper factor")
+    ax[1, 0].matshow(U_ref)
+    ax[0, 1].set_title("L_sdr_dense: SDR lower factor")
+    ax[0, 1].matshow(L_sdr_dense)
+    ax[1, 1].set_title("U_sdr_dense: SDR upper factor")
+    ax[1, 1].matshow(U_sdr_dense)
+
+    L_diff = L_ref - L_sdr_dense
+    U_diff = U_ref - U_sdr_dense
+    ax[0, 2].set_title("L: Difference between L_ref and L_sdr_dense")
+    ax[0, 2].matshow(L_diff)
+    ax[1, 2].set_title("U: Difference between U_ref and U_sdr_dense")
+    ax[1, 2].matshow(U_diff)
+    fig.colorbar(ax[0, 2].matshow(L_diff), ax=ax[0, 2], label="Relative error")
+    fig.colorbar(ax[1, 2].matshow(U_diff), ax=ax[1, 2], label="Relative error")
+    
+    plt.show() 
+    
+    assert np.allclose(L_ref, L_sdr_dense)
+    assert np.allclose(U_ref, U_sdr_dense)
+
+
+
+@pytest.mark.parametrize(
+    "nblocks, blocksize", 
+    [
+        (2, 2),
+        (10, 2),
+        (100, 2),
+        (2, 3),
+        (10, 3),
+        (100, 3),
+        (2, 100),
+        (5, 100),
+        (10, 100),
+    ]
+)
+def test_lu_decompose_tridiag(
+    nblocks: int,
+    blocksize: int,  
+):
+    symmetric = False
+    diagonal_dominant = True
+    seed = 63
+
+    A = matrix_generation.generate_tridiag_dense(
+        nblocks, blocksize, symmetric, diagonal_dominant, seed
+    )
+
+    # --- Decomposition ---
+
+    P_ref, L_ref, U_ref = la.lu(A)
+
+    if not np.allclose(P_ref, np.eye(A.shape[0])):
+        L_ref = P_ref @ L_ref
+
+    (
+        A_diagonal_blocks, 
+        A_lower_diagonal_blocks,
+        A_upper_diagonal_blocks, 
+    ) = from_dense_to_tridiagonal_arrays(A, blocksize)
+
+    (
+        L_sdr, 
+        U_sdr
+    ) = lu_factorize_tridiag(
+        A_diagonal_blocks, 
+        A_lower_diagonal_blocks,
+        A_upper_diagonal_blocks, 
+    )
+    
+    L_sdr_dense = from_tridiagonal_arrays_to_dense(
+        L_sdr[:blocksize, :], 
+        np.zeros((blocksize, (nblocks-1)*blocksize)),
+        L_sdr[blocksize:, blocksize:]
+    )
+    
+    U_sdr_dense = from_tridiagonal_arrays_to_dense(
+        U_sdr[blocksize:, :], 
+        U_sdr[:blocksize, :-blocksize],
+        np.zeros((blocksize, (nblocks-1)*blocksize)),
+    )
+
+    assert np.allclose(L_ref, L_sdr_dense)
+    assert np.allclose(U_ref, U_sdr_dense)
