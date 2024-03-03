5257b47e81a1eb4ef706cba78ca24751268402c6
vincent-maillou <vmaillou@iis.ee.ethz.ch>
2024-03-03 23:19:17 +0100

    added lu_dist example

--- examples/lu_dist/lu_dist_tridiagonal_arrowhead_example.py
+++ examples/lu_dist/lu_dist_tridiagonal_arrowhead_example.py
@@ -9,42 +9,43 @@ Copyright 2023-2024 ETH Zurich and USI. All rights reserved.
 """
 
 from sdr.utils import matrix_generation
-from sdr.utils import matrix_transform as mt
-from sdr.utils import dist_utils as du
+from sdr.utils import dist_utils
 from sdr.lu_dist.lu_dist_tridiagonal_arrowhead import lu_dist_tridiagonal_arrowhead
+from sdr.utils.matrix_transform import from_dense_to_arrowhead_arrays
 
-import copy as cp
 import numpy as np
-import matplotlib.pyplot as plt
+import copy as cp
+import pytest
 from mpi4py import MPI
 
 
-# ----- Integration test -----
 if __name__ == "__main__":
-    nblocks = 13
+    nblocks = 10
     diag_blocksize = 3
     arrow_blocksize = 2
-    symmetric = False
     diagonal_dominant = True
+    symmetric = False
     seed = 63
-
-    A = matrix_generation.generate_tridiag_arrowhead_dense(
-        nblocks, diag_blocksize, arrow_blocksize, symmetric, diagonal_dominant, seed
-    )
-
+    
     comm = MPI.COMM_WORLD
     comm_rank = comm.Get_rank()
     comm_size = comm.Get_size()
-
+    
+    if nblocks // comm_size < 3:
+        raise ValueError("Each processes should have at least 3 blocks to perfrome the middle factorization.")
+    
+    A = matrix_generation.generate_tridiag_arrowhead_dense(
+        nblocks, diag_blocksize, arrow_blocksize, symmetric, diagonal_dominant, seed
+    )
+    
     (
         start_blockrows, 
         partition_sizes, 
         end_blockrows
-    ) = du.get_partitions_indices(
+    ) = dist_utils.get_partitions_indices(
         n_partitions = comm_size, 
         total_size = nblocks - 1
     )
-
 
     # ----- Reference/Checking data -----
     A_ref = cp.deepcopy(A)
@@ -52,148 +53,118 @@ if __name__ == "__main__":
     X_ref = np.linalg.inv(A_ref)
     
     (
-        X_ref_local, 
-        X_ref_arrow_bottom, 
-        X_ref_arrow_right,
-        X_ref_arrow_tip
-    ) = du.extract_partition_tridiagonal_arrowhead_dense(
-        X_ref,
-        start_blockrows[comm_rank],
-        partition_sizes[comm_rank],
-        diag_blocksize,
-        arrow_blocksize,
+        X_ref_diagonal_blocks, 
+        X_ref_lower_diagonal_blocks, 
+        X_ref_upper_diagonal_blocks, 
+        X_ref_arrow_bottom_blocks, 
+        X_ref_arrow_right_blocks, 
+        X_ref_arrow_tip_block,
+    ) = from_dense_to_arrowhead_arrays(
+        X_ref, 
+        diag_blocksize, 
+        arrow_blocksize
     )
-
-    Bridges_upper_inv_ref, Bridges_lower_inv_ref = du.extract_bridges_tridiagonal_dense(
-        X_ref, diag_blocksize, start_blockrows
-    )
-
-    X_ref_local = mt.cut_to_blocktridiag(X_ref_local, diag_blocksize)
-    # ----- Reference/Checking data -----
-
 
     (
-        A_local, 
-        A_arrow_bottom, 
-        A_arrow_right,
-        A_arrow_tip
-    ) = du.extract_partition_tridiagonal_arrowhead_dense(
-        A,
+        X_ref_diagonal_blocks_local, 
+        X_ref_lower_diagonal_blocks_local, 
+        X_ref_upper_diagonal_blocks_local, 
+        X_ref_arrow_bottom_blocks_local, 
+        X_ref_arrow_right_blocks_local, 
+        X_ref_arrow_tip_block_local
+    ) = dist_utils.extract_partition_tridiagonal_arrowhead_array(
+        X_ref_diagonal_blocks, 
+        X_ref_lower_diagonal_blocks, 
+        X_ref_upper_diagonal_blocks, 
+        X_ref_arrow_bottom_blocks, 
+        X_ref_arrow_right_blocks, 
+        X_ref_arrow_tip_block,
         start_blockrows[comm_rank],
-        partition_sizes[comm_rank],
-        diag_blocksize,
-        arrow_blocksize,
+        partition_sizes[comm_rank]
     )
-    
-    Bridges_upper, Bridges_lower = du.extract_bridges_tridiagonal_dense(
-        A, diag_blocksize, start_blockrows
-    )
-    
-    
-    fig, axs = plt.subplots(2, 5)
-    fig.suptitle("Initial datas, process: " + str(comm_rank))
-    axs[0, 0].matshow(A)
-    axs[0, 0].set_title("A")
-    axs[0, 1].matshow(A_local)
-    axs[0, 1].set_title("A_local")
-    axs[0, 2].matshow(A_arrow_bottom)
-    axs[0, 2].set_title("A_arrow_bottom")
-    axs[0, 3].matshow(A_arrow_right)
-    axs[0, 3].set_title("A_arrow_right")
-    axs[0, 4].matshow(A_arrow_tip)
-    axs[0, 4].set_title("A_arrow_tip")
-    
-    axs[1, 0].matshow(X_ref)
-    axs[1, 0].set_title("X_ref")
-    axs[1, 1].matshow(X_ref_local)
-    axs[1, 1].set_title("X_ref_local")
-    axs[1, 2].matshow(X_ref_arrow_bottom)
-    axs[1, 2].set_title("X_ref_arrow_bottom")
-    axs[1, 3].matshow(X_ref_arrow_right)
-    axs[1, 3].set_title("X_ref_arrow_right")
-    axs[1, 4].matshow(X_ref_arrow_tip)
-    axs[1, 4].set_title("X_ref_arrow_tip")
-    plt.show()
-
 
     (
-        X_local, 
-        X_bridges_upper, 
-        X_bridges_lower, 
-        X_arrow_bottom, 
-        X_arrow_right, 
-        X_global_arrow_tip
+        X_ref_bridges_lower,
+        X_ref_bridges_upper
+    ) = dist_utils.extract_bridges_tridiagonal_array(
+        X_ref_lower_diagonal_blocks,
+        X_ref_upper_diagonal_blocks, 
+        start_blockrows
+    )
+    # -----------------------------------
+
+    (
+        A_diagonal_blocks, 
+        A_lower_diagonal_blocks, 
+        A_upper_diagonal_blocks, 
+        A_arrow_bottom_blocks, 
+        A_arrow_right_blocks, 
+        A_arrow_tip_block,
+    ) = from_dense_to_arrowhead_arrays(
+        A, 
+        diag_blocksize, 
+        arrow_blocksize
+    )
+
+    (
+        A_diagonal_blocks_local, 
+        A_lower_diagonal_blocks_local, 
+        A_upper_diagonal_blocks_local, 
+        A_arrow_bottom_blocks_local, 
+        A_arrow_right_blocks_local, 
+        A_arrow_tip_block_local
+    ) = dist_utils.extract_partition_tridiagonal_arrowhead_array(
+        A_diagonal_blocks, 
+        A_lower_diagonal_blocks, 
+        A_upper_diagonal_blocks, 
+        A_arrow_bottom_blocks, 
+        A_arrow_right_blocks, 
+        A_arrow_tip_block,
+        start_blockrows[comm_rank],
+        partition_sizes[comm_rank]
+    )
+
+    (
+        A_bridges_lower,
+        A_bridges_upper
+    ) = dist_utils.extract_bridges_tridiagonal_array(
+        A_lower_diagonal_blocks, 
+        A_upper_diagonal_blocks,  
+        start_blockrows
+    )
+
+    (
+        X_diagonal_blocks_local, 
+        X_lower_diagonal_blocks_local, 
+        X_upper_diagonal_blocks_local, 
+        X_arrow_bottom_blocks_local, 
+        X_arrow_right_blocks_local, 
+        X_arrow_tip_block_local,
+        X_bridges_lower,
+        X_bridges_upper
     ) = lu_dist_tridiagonal_arrowhead(
-        A_local,
-        A_arrow_bottom,
-        A_arrow_right,
-        A_arrow_tip,
-        Bridges_upper,
-        Bridges_lower,
-        diag_blocksize,
-        arrow_blocksize,
+        A_diagonal_blocks_local, 
+        A_lower_diagonal_blocks_local, 
+        A_upper_diagonal_blocks_local, 
+        A_arrow_bottom_blocks_local, 
+        A_arrow_right_blocks_local, 
+        A_arrow_tip_block_local,
+        A_bridges_lower,
+        A_bridges_upper
     )
 
-    X_local_cut_tridiag = mt.cut_to_blocktridiag(X_local, diag_blocksize)
-
-    fig, axs = plt.subplots(2, 4)
-    fig.suptitle("Results, process: " + str(comm_rank))
-    axs[0, 0].matshow(X_ref_local)
-    axs[0, 0].set_title("X_ref_local")
-    axs[0, 1].matshow(X_ref_arrow_bottom)
-    axs[0, 1].set_title("X_ref_arrow_bottom")
-    axs[0, 2].matshow(X_ref_arrow_right)
-    axs[0, 2].set_title("X_ref_arrow_right")
-    axs[0, 3].matshow(X_ref_arrow_tip)
-    axs[0, 3].set_title("X_ref_arrow_tip")
-    
-    axs[1, 0].matshow(X_local_cut_tridiag)
-    axs[1, 0].set_title("X_local_cut_tridiag")
-    axs[1, 1].matshow(X_arrow_bottom)
-    axs[1, 1].set_title("X_arrow_bottom")
-    axs[1, 2].matshow(X_arrow_right)
-    axs[1, 2].set_title("X_arrow_right")
-    axs[1, 3].matshow(X_global_arrow_tip)
-    axs[1, 3].set_title("X_global_arrow_tip")
-    plt.show()
-
-    # ----- VERFIFYING THE RESULTS -----
-
-    assert np.allclose(X_ref_local, X_local_cut_tridiag)
-    norme_diff_partition_i = np.linalg.norm(X_ref_local - X_local_cut_tridiag)
-    print("Partition n", comm_rank, "  norm diff = ", norme_diff_partition_i)
-    
-    assert np.allclose(X_ref_arrow_bottom, X_arrow_bottom)
-    norme_diff_arrow_bottom_i = np.linalg.norm(X_ref_arrow_bottom - X_arrow_bottom)
-    print("Arrow bottom n", comm_rank, "  norm diff = ", norme_diff_arrow_bottom_i)
-    
-    assert np.allclose(X_ref_arrow_right, X_arrow_right)
-    norme_diff_arrow_right_i = np.linalg.norm(X_ref_arrow_right - X_arrow_right)
-    print("Arrow right n", comm_rank, "  norm diff = ", norme_diff_arrow_right_i)
-    
-    assert np.allclose(X_ref_arrow_tip, X_global_arrow_tip)
-    norme_diff_arrow_tip_i = np.linalg.norm(X_ref_arrow_tip - X_global_arrow_tip)
-    print("Arrow tip n", comm_rank, "  norm diff = ", norme_diff_arrow_tip_i)
+    assert np.allclose(X_ref_diagonal_blocks_local, X_diagonal_blocks_local)
+    assert np.allclose(X_ref_lower_diagonal_blocks_local, X_lower_diagonal_blocks_local)
+    assert np.allclose(X_ref_upper_diagonal_blocks_local, X_upper_diagonal_blocks_local)
+    assert np.allclose(X_ref_arrow_bottom_blocks_local, X_arrow_bottom_blocks_local)
+    assert np.allclose(X_ref_arrow_right_blocks_local, X_arrow_right_blocks_local)
+    assert np.allclose(X_ref_arrow_tip_block_local, X_arrow_tip_block_local)
 
     # Check for bridges correctness
-    print("Bridges correctness:")
     if comm_rank == 0:
-        norme_diff_upper_bridge_i = np.linalg.norm(Bridges_upper_inv_ref[comm_rank] - X_bridges_upper[comm_rank])
-        assert np.allclose(Bridges_upper_inv_ref[comm_rank], X_bridges_upper[comm_rank])
-        print("    Upper bridge n", comm_rank, "  norm diff = ", norme_diff_upper_bridge_i)            
-                        
+        assert np.allclose(X_ref_bridges_upper[:, comm_rank * diag_blocksize : (comm_rank + 1) * diag_blocksize], X_bridges_upper[:, comm_rank * diag_blocksize : (comm_rank + 1) * diag_blocksize])
     elif comm_rank == comm_size-1:
-        norme_diff_lower_bridge_i = np.linalg.norm(Bridges_lower_inv_ref[comm_rank-1] - X_bridges_lower[comm_rank-1])
-        assert np.allclose(Bridges_lower_inv_ref[comm_rank-1], X_bridges_lower[comm_rank-1])
-        print("    Lower bridge n", comm_rank, "  norm diff = ", norme_diff_lower_bridge_i)
-        
+        assert np.allclose(X_ref_bridges_lower[:, (comm_rank - 1) * diag_blocksize : comm_rank * diag_blocksize], X_bridges_lower[:, (comm_rank - 1) * diag_blocksize : comm_rank * diag_blocksize])
     else:
-        norme_diff_upper_bridge_i = np.linalg.norm(Bridges_upper_inv_ref[comm_rank] - X_bridges_upper[comm_rank])
-        assert np.allclose(Bridges_upper_inv_ref[comm_rank], X_bridges_upper[comm_rank])
-        print("    Upper bridge n", comm_rank, "  norm diff = ", norme_diff_upper_bridge_i)            
-        
-        norme_diff_lower_bridge_i = np.linalg.norm(Bridges_lower_inv_ref[comm_rank-1] - X_bridges_lower[comm_rank-1])
-        assert np.allclose(Bridges_lower_inv_ref[comm_rank-1], X_bridges_lower[comm_rank-1])
-        print("    Lower bridge n", comm_rank, "  norm diff = ", norme_diff_lower_bridge_i)
-        
-        
+        assert np.allclose(X_ref_bridges_upper[:, comm_rank * diag_blocksize : (comm_rank + 1) * diag_blocksize], X_bridges_upper[:, comm_rank * diag_blocksize : (comm_rank + 1) * diag_blocksize])
+        assert np.allclose(X_ref_bridges_lower[:, (comm_rank - 1) * diag_blocksize : comm_rank * diag_blocksize], X_bridges_lower[:, (comm_rank - 1) * diag_blocksize : comm_rank * diag_blocksize])
