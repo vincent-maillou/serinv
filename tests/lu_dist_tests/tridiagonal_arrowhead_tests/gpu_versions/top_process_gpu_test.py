fec9a5c52d3be250906e49b1194a9ecb54b89a12
vincent-maillou <vmaillou@iis.ee.ethz.ch>
2024-03-18 18:52:24 +0100

    cupy handling

--- tests/lu_dist_tests/tridiagonal_arrowhead_tests/gpu_versions/top_process_gpu_test.py
+++ tests/lu_dist_tests/tridiagonal_arrowhead_tests/gpu_versions/top_process_gpu_test.py
@@ -15,161 +15,158 @@ import numpy as np
 import pytest
 
 try:
-    import cupy
+    from sdr.lu_dist.lu_dist_tridiagonal_arrowhead_gpu import (
+        top_factorize_gpu,
+        top_sinv_gpu,
+    )
+    from sdr.utils.matrix_generation import generate_tridiag_arrowhead_dense
+    from sdr.utils.matrix_transform import from_dense_to_arrowhead_arrays
+
+    @pytest.mark.gpu
+    @pytest.mark.mpi_skip()
+    @pytest.mark.parametrize(
+        "nblocks, diag_blocksize, arrow_blocksize",
+        [
+            (2, 2, 2),
+            (2, 3, 2),
+            (2, 2, 3),
+            (10, 2, 2),
+            (10, 3, 2),
+            (10, 2, 3),
+            (10, 10, 2),
+            (10, 2, 10),
+        ],
+    )
+    def test_lu_dist_top_process(
+        nblocks: int,
+        diag_blocksize: int,
+        arrow_blocksize: int,
+    ):
+        diagonal_dominant = True
+        symmetric = False
+        seed = 63
+
+        A = generate_tridiag_arrowhead_dense(
+            nblocks, diag_blocksize, arrow_blocksize, symmetric, diagonal_dominant, seed
+        )
+
+        # ----- Reference -----
+        A_ref = deepcopy(A)
+
+        X_ref = np.linalg.inv(A_ref)
+
+        (
+            X_ref_diagonal_blocks,
+            X_ref_lower_diagonal_blocks,
+            X_ref_upper_diagonal_blocks,
+            X_ref_arrow_bottom_blocks,
+            X_ref_arrow_right_blocks,
+            X_ref_arrow_tip_block,
+        ) = from_dense_to_arrowhead_arrays(X_ref, diag_blocksize, arrow_blocksize)
+        # ---------------------
+
+        (
+            A_diagonal_blocks,
+            A_lower_diagonal_blocks,
+            A_upper_diagonal_blocks,
+            A_arrow_bottom_blocks,
+            A_arrow_right_blocks,
+            A_arrow_tip_block,
+        ) = from_dense_to_arrowhead_arrays(A, diag_blocksize, arrow_blocksize)
+
+        (
+            L_diagonal_blocks,
+            L_lower_diagonal_blocks,
+            L_arrow_bottom_blocks,
+            U_diagonal_blocks,
+            U_upper_diagonal_blocks,
+            U_arrow_right_blocks,
+            Update_arrow_tip,
+            A_diagonal_blocks_local_updated,
+            A_arrow_bottom_blocks_local_updated,
+            A_arrow_right_blocks_local_updated,
+        ) = top_factorize_gpu(
+            A_diagonal_blocks,
+            A_lower_diagonal_blocks,
+            A_upper_diagonal_blocks,
+            A_arrow_bottom_blocks,
+            A_arrow_right_blocks,
+            A_arrow_tip_block,
+        )
+
+        # Create and inverse the reduced system created by the last reduced block
+        # and the tip of the arrowhead.
+
+        reduced_system = np.zeros(
+            (diag_blocksize + arrow_blocksize, diag_blocksize + arrow_blocksize)
+        )
+        reduced_system[0:diag_blocksize, 0:diag_blocksize] = (
+            A_diagonal_blocks_local_updated[:, :]
+        )
+        reduced_system[-arrow_blocksize:, -arrow_blocksize:] = (
+            A_arrow_tip_block + Update_arrow_tip
+        )
+        reduced_system[0:diag_blocksize, -arrow_blocksize:] = (
+            A_arrow_right_blocks_local_updated[:, :]
+        )
+        reduced_system[-arrow_blocksize:, 0:diag_blocksize] = (
+            A_arrow_bottom_blocks_local_updated[:, :]
+        )
+
+        reduced_system_inv = np.linalg.inv(reduced_system)
+
+        X_sdr_diagonal_blocks = np.zeros_like(A_diagonal_blocks)
+        X_sdr_lower_diagonal_blocks = np.zeros_like(A_lower_diagonal_blocks)
+        X_sdr_upper_diagonal_blocks = np.zeros_like(A_upper_diagonal_blocks)
+        X_sdr_arrow_bottom_blocks = np.zeros_like(A_arrow_bottom_blocks)
+        X_sdr_arrow_right_blocks = np.zeros_like(A_arrow_right_blocks)
+        X_sdr_global_arrow_tip = np.zeros_like(A_arrow_tip_block)
+
+        X_sdr_diagonal_blocks[:, -diag_blocksize:] = reduced_system_inv[
+            0:diag_blocksize, 0:diag_blocksize
+        ]
+        X_sdr_arrow_bottom_blocks[:, -diag_blocksize:] = reduced_system_inv[
+            -arrow_blocksize:, 0:diag_blocksize
+        ]
+        X_sdr_arrow_right_blocks[-diag_blocksize:, :] = reduced_system_inv[
+            0:diag_blocksize, -arrow_blocksize:
+        ]
+        X_sdr_global_arrow_tip = reduced_system_inv[
+            -arrow_blocksize:, -arrow_blocksize:
+        ]
+
+        # ----- Selected inversion part -----
+        (
+            X_sdr_diagonal_blocks,
+            X_sdr_lower_diagonal_blocks,
+            X_sdr_upper_diagonal_blocks,
+            X_sdr_arrow_bottom_blocks,
+            X_sdr_arrow_right_blocks,
+            X_sdr_global_arrow_tip,
+        ) = top_sinv_gpu(
+            X_sdr_diagonal_blocks,
+            X_sdr_lower_diagonal_blocks,
+            X_sdr_upper_diagonal_blocks,
+            X_sdr_arrow_bottom_blocks,
+            X_sdr_arrow_right_blocks,
+            X_sdr_global_arrow_tip,
+            L_diagonal_blocks,
+            L_lower_diagonal_blocks,
+            L_arrow_bottom_blocks,
+            U_diagonal_blocks,
+            U_upper_diagonal_blocks,
+            U_arrow_right_blocks,
+        )
+
+        assert np.allclose(X_ref_diagonal_blocks, X_sdr_diagonal_blocks)
+        assert np.allclose(X_ref_lower_diagonal_blocks, X_sdr_lower_diagonal_blocks)
+        assert np.allclose(X_ref_upper_diagonal_blocks, X_sdr_upper_diagonal_blocks)
+        assert np.allclose(X_ref_arrow_bottom_blocks, X_sdr_arrow_bottom_blocks)
+        assert np.allclose(X_ref_arrow_right_blocks, X_sdr_arrow_right_blocks)
+        assert np.allclose(X_ref_arrow_tip_block, X_sdr_global_arrow_tip)
+
 except ImportError:
-    import sys
-
-    sys.exit()
-
-from sdr.lu_dist.lu_dist_tridiagonal_arrowhead_gpu import (
-    top_factorize_gpu,
-    top_sinv_gpu,
-)
-from sdr.utils.matrix_generation import generate_tridiag_arrowhead_dense
-from sdr.utils.matrix_transform import from_dense_to_arrowhead_arrays
-
-
-@pytest.mark.gpu
-@pytest.mark.mpi_skip()
-@pytest.mark.parametrize(
-    "nblocks, diag_blocksize, arrow_blocksize",
-    [
-        (2, 2, 2),
-        (2, 3, 2),
-        (2, 2, 3),
-        (10, 2, 2),
-        (10, 3, 2),
-        (10, 2, 3),
-        (10, 10, 2),
-        (10, 2, 10),
-    ],
-)
-def test_lu_dist_top_process(
-    nblocks: int,
-    diag_blocksize: int,
-    arrow_blocksize: int,
-):
-    diagonal_dominant = True
-    symmetric = False
-    seed = 63
-
-    A = generate_tridiag_arrowhead_dense(
-        nblocks, diag_blocksize, arrow_blocksize, symmetric, diagonal_dominant, seed
-    )
-
-    # ----- Reference -----
-    A_ref = deepcopy(A)
-
-    X_ref = np.linalg.inv(A_ref)
-
-    (
-        X_ref_diagonal_blocks,
-        X_ref_lower_diagonal_blocks,
-        X_ref_upper_diagonal_blocks,
-        X_ref_arrow_bottom_blocks,
-        X_ref_arrow_right_blocks,
-        X_ref_arrow_tip_block,
-    ) = from_dense_to_arrowhead_arrays(X_ref, diag_blocksize, arrow_blocksize)
-    # ---------------------
-
-    (
-        A_diagonal_blocks,
-        A_lower_diagonal_blocks,
-        A_upper_diagonal_blocks,
-        A_arrow_bottom_blocks,
-        A_arrow_right_blocks,
-        A_arrow_tip_block,
-    ) = from_dense_to_arrowhead_arrays(A, diag_blocksize, arrow_blocksize)
-
-    (
-        L_diagonal_blocks,
-        L_lower_diagonal_blocks,
-        L_arrow_bottom_blocks,
-        U_diagonal_blocks,
-        U_upper_diagonal_blocks,
-        U_arrow_right_blocks,
-        Update_arrow_tip,
-        A_diagonal_blocks_local_updated,
-        A_arrow_bottom_blocks_local_updated,
-        A_arrow_right_blocks_local_updated,
-    ) = top_factorize_gpu(
-        A_diagonal_blocks,
-        A_lower_diagonal_blocks,
-        A_upper_diagonal_blocks,
-        A_arrow_bottom_blocks,
-        A_arrow_right_blocks,
-        A_arrow_tip_block,
-    )
-
-    # Create and inverse the reduced system created by the last reduced block
-    # and the tip of the arrowhead.
-
-    reduced_system = np.zeros(
-        (diag_blocksize + arrow_blocksize, diag_blocksize + arrow_blocksize)
-    )
-    reduced_system[
-        0:diag_blocksize, 0:diag_blocksize
-    ] = A_diagonal_blocks_local_updated[:, :]
-    reduced_system[-arrow_blocksize:, -arrow_blocksize:] = (
-        A_arrow_tip_block + Update_arrow_tip
-    )
-    reduced_system[
-        0:diag_blocksize, -arrow_blocksize:
-    ] = A_arrow_right_blocks_local_updated[:, :]
-    reduced_system[
-        -arrow_blocksize:, 0:diag_blocksize
-    ] = A_arrow_bottom_blocks_local_updated[:, :]
-
-    reduced_system_inv = np.linalg.inv(reduced_system)
-
-    X_sdr_diagonal_blocks = np.zeros_like(A_diagonal_blocks)
-    X_sdr_lower_diagonal_blocks = np.zeros_like(A_lower_diagonal_blocks)
-    X_sdr_upper_diagonal_blocks = np.zeros_like(A_upper_diagonal_blocks)
-    X_sdr_arrow_bottom_blocks = np.zeros_like(A_arrow_bottom_blocks)
-    X_sdr_arrow_right_blocks = np.zeros_like(A_arrow_right_blocks)
-    X_sdr_global_arrow_tip = np.zeros_like(A_arrow_tip_block)
-
-    X_sdr_diagonal_blocks[:, -diag_blocksize:] = reduced_system_inv[
-        0:diag_blocksize, 0:diag_blocksize
-    ]
-    X_sdr_arrow_bottom_blocks[:, -diag_blocksize:] = reduced_system_inv[
-        -arrow_blocksize:, 0:diag_blocksize
-    ]
-    X_sdr_arrow_right_blocks[-diag_blocksize:, :] = reduced_system_inv[
-        0:diag_blocksize, -arrow_blocksize:
-    ]
-    X_sdr_global_arrow_tip = reduced_system_inv[-arrow_blocksize:, -arrow_blocksize:]
-
-    # ----- Selected inversion part -----
-    (
-        X_sdr_diagonal_blocks,
-        X_sdr_lower_diagonal_blocks,
-        X_sdr_upper_diagonal_blocks,
-        X_sdr_arrow_bottom_blocks,
-        X_sdr_arrow_right_blocks,
-        X_sdr_global_arrow_tip,
-    ) = top_sinv_gpu(
-        X_sdr_diagonal_blocks,
-        X_sdr_lower_diagonal_blocks,
-        X_sdr_upper_diagonal_blocks,
-        X_sdr_arrow_bottom_blocks,
-        X_sdr_arrow_right_blocks,
-        X_sdr_global_arrow_tip,
-        L_diagonal_blocks,
-        L_lower_diagonal_blocks,
-        L_arrow_bottom_blocks,
-        U_diagonal_blocks,
-        U_upper_diagonal_blocks,
-        U_arrow_right_blocks,
-    )
-
-    assert np.allclose(X_ref_diagonal_blocks, X_sdr_diagonal_blocks)
-    assert np.allclose(X_ref_lower_diagonal_blocks, X_sdr_lower_diagonal_blocks)
-    assert np.allclose(X_ref_upper_diagonal_blocks, X_sdr_upper_diagonal_blocks)
-    assert np.allclose(X_ref_arrow_bottom_blocks, X_sdr_arrow_bottom_blocks)
-    assert np.allclose(X_ref_arrow_right_blocks, X_sdr_arrow_right_blocks)
-    assert np.allclose(X_ref_arrow_tip_block, X_sdr_global_arrow_tip)
-
+    pass
 
 if __name__ == "__main__":
     test_lu_dist_top_process(10, 10, 2)
