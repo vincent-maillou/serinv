111bbb86a36da925b0c9e8458aa5c160c60d655f
vincent-maillou <vmaillou@iis.ee.ethz.ch>
2024-03-21 19:25:17 +0100

    modified data generation so that it scales..

--- bench/performances/lu_dist/lu_dist_tridiagonal_arrowhead_performances.py
+++ bench/performances/lu_dist/lu_dist_tridiagonal_arrowhead_performances.py
@@ -43,19 +43,21 @@ if __name__ == "__main__":
     comm_rank = comm.Get_rank()
     comm_size = comm.Get_size()
 
-    for nblocks in l_nblocks:
+    partition_sizes = [l_nblocks[i] // comm_size for i in range(len(l_nblocks))]
+
+    for partition_size in partition_sizes:
         for diag_blocksize in l_diag_blocksize:
             for arrow_blocksize in l_arrow_blocksize:
 
-                if nblocks // comm_size < 3:
+                if partition_size < 3:
                     raise ValueError(
                         "Each processes should have at least 3 blocks to perfrome the middle factorization."
                     )
 
                 if comm_rank == 0:
                     print(
-                        "Starting data generation for:\n    nblocks: ",
-                        nblocks,
+                        "Starting data generation for:\n    partition_size: ",
+                        partition_size,
                         " diag_blocksize: ",
                         diag_blocksize,
                         " arrow_blocksize: ",
@@ -63,20 +65,42 @@ if __name__ == "__main__":
                     )
 
                 t_data_gen_start = time.perf_counter_ns()
-                (
-                    A_diagonal_blocks_ref,
-                    A_lower_diagonal_blocks_ref,
-                    A_upper_diagonal_blocks_ref,
-                    A_arrow_bottom_blocks_ref,
-                    A_arrow_right_blocks_ref,
-                    A_arrow_tip_block_ref,
-                ) = generate_tridiag_arrowhead_arrays(
-                    nblocks,
-                    diag_blocksize,
+                A_diagonal_blocks_ref = np.random.rand(
+                    diag_blocksize, partition_size * diag_blocksize
+                )
+                A_lower_diagonal_blocks_ref = np.random.rand(
+                    diag_blocksize, (partition_size - 1) * diag_blocksize
+                )
+                A_upper_diagonal_blocks_ref = np.random.rand(
+                    diag_blocksize, (partition_size - 1) * diag_blocksize
+                )
+                A_arrow_bottom_blocks_ref = np.random.rand(
+                    arrow_blocksize, partition_size * diag_blocksize
+                )
+                A_arrow_right_blocks_ref = np.random.rand(
+                    partition_size * diag_blocksize,
                     arrow_blocksize,
-                    symmetric,
-                    diagonal_dominant,
-                    seed,
+                )
+
+                # Make diagonally dominant
+                for i in range(partition_size):
+                    A_diagonal_blocks_ref[
+                        :, i * diag_blocksize : (i + 1) * diag_blocksize
+                    ] += np.eye(diag_blocksize) * (3 * diag_blocksize + arrow_blocksize)
+
+                matrix_elements_size = (
+                    partition_size * comm_size * diag_blocksize + arrow_blocksize
+                )
+                A_arrow_tip_block_ref = (
+                    np.random.rand(arrow_blocksize, arrow_blocksize)
+                    + np.eye(arrow_blocksize) * matrix_elements_size
+                )
+
+                A_bridges_lower = np.random.rand(
+                    diag_blocksize, (comm_size - 1) * diag_blocksize
+                )
+                A_bridges_upper = np.random.rand(
+                    diag_blocksize, (comm_size - 1) * diag_blocksize
                 )
                 t_data_gen_stop = time.perf_counter_ns()
                 t_data_gen = t_data_gen_stop - t_data_gen_start
@@ -91,9 +115,15 @@ if __name__ == "__main__":
                     * A_arrow_bottom_blocks_ref.itemsize
                     + A_arrow_right_blocks_ref.size * A_arrow_right_blocks_ref.itemsize
                     + A_arrow_tip_block_ref.size * A_arrow_tip_block_ref.itemsize
+                    + A_bridges_lower.size * A_bridges_lower.itemsize
+                    + A_bridges_upper.size * A_bridges_upper.itemsize
                 )
                 if comm_rank == 0:
-                    print("    Input data memory size: ", input_mem_size * 1e-9, "GB")
+                    print(
+                        "    Input (partition_size) memory size: ",
+                        input_mem_size * 1e-9,
+                        "GB",
+                    )
                     print("    Data generation took: ", t_data_gen * 1e-9, "s")
 
                 headers = {}
@@ -102,7 +132,9 @@ if __name__ == "__main__":
                 headers["MKL_NUM_THREADS"] = mkl.get_max_threads()
                 headers["COMM_SIZE"] = comm_size
                 headers["COMM_RANK"] = comm_rank
-                headers["nblocks"] = nblocks
+                headers["nblocks"] = partition_size * comm_size
+                headers["partition_size"] = partition_size
+                headers["partition_memory"] = input_mem_size * 1e-9
                 headers["blocksize"] = diag_blocksize
                 headers["arrow_blocksize"] = arrow_blocksize
                 headers["symmetric"] = symmetric
@@ -111,12 +143,14 @@ if __name__ == "__main__":
                 headers["total_runtime"] = 0.0
 
                 for i in range(N_WARMUPS + N_RUNS):
-                    A_diagonal_blocks = A_diagonal_blocks_ref.copy()
-                    A_lower_diagonal_blocks = A_lower_diagonal_blocks_ref.copy()
-                    A_upper_diagonal_blocks = A_upper_diagonal_blocks_ref.copy()
-                    A_arrow_bottom_blocks = A_arrow_bottom_blocks_ref.copy()
-                    A_arrow_right_blocks = A_arrow_right_blocks_ref.copy()
-                    A_arrow_tip_block = A_arrow_tip_block_ref.copy()
+                    A_diagonal_blocks_local = A_diagonal_blocks_ref.copy()
+                    A_lower_diagonal_blocks_local = A_lower_diagonal_blocks_ref.copy()
+                    A_upper_diagonal_blocks_local = A_upper_diagonal_blocks_ref.copy()
+                    A_arrow_bottom_blocks_local = A_arrow_bottom_blocks_ref.copy()
+                    A_arrow_right_blocks_local = A_arrow_right_blocks_ref.copy()
+                    A_arrow_tip_block_local = A_arrow_tip_block_ref.copy()
+                    A_bridges_lower_local = A_bridges_lower.copy()
+                    A_bridges_upper_local = A_bridges_upper.copy()
 
                     if comm_rank == 0:
                         if i < N_WARMUPS:
@@ -129,40 +163,6 @@ if __name__ == "__main__":
                                 end="",
                                 flush=True,
                             )
-
-                    (
-                        start_blockrows,
-                        partition_sizes,
-                        end_blockrows,
-                    ) = get_partitions_indices(
-                        n_partitions=comm_size, total_size=nblocks - 1
-                    )
-
-                    (
-                        A_diagonal_blocks_local,
-                        A_lower_diagonal_blocks_local,
-                        A_upper_diagonal_blocks_local,
-                        A_arrow_bottom_blocks_local,
-                        A_arrow_right_blocks_local,
-                        A_arrow_tip_block_local,
-                    ) = extract_partition_tridiagonal_arrowhead_array(
-                        A_diagonal_blocks,
-                        A_lower_diagonal_blocks,
-                        A_upper_diagonal_blocks,
-                        A_arrow_bottom_blocks,
-                        A_arrow_right_blocks,
-                        A_arrow_tip_block,
-                        start_blockrows[comm_rank],
-                        partition_sizes[comm_rank],
-                    )
-
-                    (A_bridges_lower, A_bridges_upper) = (
-                        extract_bridges_tridiagonal_array(
-                            A_lower_diagonal_blocks,
-                            A_upper_diagonal_blocks,
-                            start_blockrows,
-                        )
-                    )
 
                     comm.Barrier()
                     t_run_start = time.perf_counter_ns()
@@ -184,8 +184,8 @@ if __name__ == "__main__":
                         A_arrow_bottom_blocks_local,
                         A_arrow_right_blocks_local,
                         A_arrow_tip_block_local,
-                        A_bridges_lower,
-                        A_bridges_upper,
+                        A_bridges_lower_local,
+                        A_bridges_upper_local,
                     )
                     comm.Barrier()
                     t_run_stop = time.perf_counter_ns()
