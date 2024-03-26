5b9909e289c74969f2be059b1d03f2b3907a10c9
vincent-maillou <vmaillou@iis.ee.ethz.ch>
2024-03-26 16:49:42 +0100

    Added daint running scripts

---
+++ bench/bash_scripts/Daint_scripts/run_daint_gpu_sequential.sh
@@ -0,0 +1,21 @@
+#!/bin/bash -l
+#SBATCH --job-name="daint_gpu_sequential"
+#SBATCH --account="s1212"
+#SBATCH --mail-type=ALL
+#SBATCH --mail-user=vmaillou@iis.ee.ethz.ch
+#SBATCH --time=00:05:00
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-core=1
+#SBATCH --ntasks-per-node=12
+#SBATCH --cpus-per-task=1
+#SBATCH --partition=normal
+#SBATCH --constraint=gpu
+#SBATCH --hint=nomultithread
+
+export OMP_NUM_THREADS=$SLURM_CPUS_PER_TASK
+export CRAY_CUDA_MPS=1
+
+file=../../performances/lu/gpu_versions/lu_factorize_tridiag_arrowhead_gpu_performances.py
+# file=../../performances/lu/gpu_versions/lu_sinv_tridiag_arrowhead_gpu_performances.py
+
+srun python ${file}
