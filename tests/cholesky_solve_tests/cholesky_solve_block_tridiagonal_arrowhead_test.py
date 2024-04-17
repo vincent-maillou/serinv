09570b9496dca4b7ed4d84ccfb2af6fe83b07c9c
vincent-maillou <vmaillou@iis.ee.ethz.ch>
2024-04-17 19:30:39 +0200

    forward pass close but not working

--- tests/cholesky_solve_tests/cholesky_solve_block_tridiagonal_arrowhead_test.py
+++ tests/cholesky_solve_tests/cholesky_solve_block_tridiagonal_arrowhead_test.py
@@ -10,17 +10,23 @@ Copyright 2023-2024 ETH Zurich and USI. All rights reserved.
 
 import matplotlib.pyplot as plt
 import numpy as np
+import numpy.linalg as npla
 import pytest
 import scipy.linalg as la
 
-from sdr.cholesky.cholesky_factorize import chol_dcmp_tridiag_arrowhead
-from sdr.cholesky.cholesky_solve import chol_slv_tridiag_arrowhead
+from sdr.cholesky.cholesky_factorize import (
+    cholesky_factorize_block_tridiagonal_arrowhead,
+)
+from sdr.cholesky.cholesky_solve import cholesky_solve_block_tridiagonal_arrowhead
 from sdr.utils import matrix_generation_dense
+from sdr.utils.matrix_transformation_arrays import (
+    convert_block_tridiagonal_arrowhead_dense_to_arrays,
+)
 
 # Testing of block tridiagonal arrowhead cholesky
 if __name__ == "__main__":
     nblocks = 5
-    diag_blocksize = 6
+    diag_blocksize = 4
     arrow_blocksize = 2
     symmetric = True
     diagonal_dominant = True
@@ -30,47 +36,47 @@ if __name__ == "__main__":
         nblocks, diag_blocksize, arrow_blocksize, symmetric, diagonal_dominant, seed
     )
 
-    L_ref = la.cholesky(A, lower=True)
-    L_sdr = chol_dcmp_tridiag_arrowhead(A, diag_blocksize, arrow_blocksize)
+    (
+        A_diagonal_blocks,
+        A_lower_diagonal_blocks,
+        _,
+        A_arrow_bottom_blocks,
+        _,
+        A_arrow_tip_block,
+    ) = convert_block_tridiagonal_arrowhead_dense_to_arrays(
+        A, diag_blocksize, arrow_blocksize
+    )
+
+    (
+        L_diagonal_blocks,
+        L_lower_diagonal_blocks,
+        L_arrow_bottom_blocks,
+        L_arrow_tip_block,
+    ) = cholesky_factorize_block_tridiagonal_arrowhead(
+        A_diagonal_blocks,
+        A_lower_diagonal_blocks,
+        A_arrow_bottom_blocks,
+        A_arrow_tip_block,
+    )
 
     n_rhs = 3
     B = np.random.randn(A.shape[0], n_rhs)
 
-    # --- Solving ---
+    X_ref = npla.solve(A, B)
 
-    # X_ref = la.cho_solve((L_ref, True), B)
-    # Is equivalent to..
-    Y_ref = la.solve_triangular(L_ref, B, lower=True)
-    X_ref = la.solve_triangular(L_ref.T, Y_ref, lower=False)
-
-    fig, ax = plt.subplots(1, 3)
-    ax[0].set_title("X_ref: Reference cholesky solver")
-    ax[0].matshow(X_ref)
-
-    X_sdr = chol_slv_tridiag_arrowhead(L_sdr, B, diag_blocksize, arrow_blocksize)
-    ax[1].set_title("X_sdr: Selected cholesky solver")
-    ax[1].matshow(X_sdr)
-
-    X_diff = X_ref - X_sdr
-    ax[2].set_title("X_diff: Difference between X_ref and X_sdr")
-    ax[2].matshow(X_diff)
-    fig.colorbar(ax[2].matshow(X_diff), ax=ax[2], label="Relative error", shrink=0.4)
-
-    plt.show()
-
-    # X_ref = la.cho_solve((L_ref, True), B)
-    # Is equivalent to..
-    Y_ref = la.solve_triangular(L_ref, B, lower=True)
-    X_ref = la.solve_triangular(L_ref.T, Y_ref, lower=False)
-
-    fig, ax = plt.subplots(1, 3)
-    ax[0].set_title("X_ref: Reference cholesky solver")
-    ax[0].matshow(X_ref)
-
-    X_sdr = chol_slv_tridiag_arrowhead(
-        L_sdr, B, diag_blocksize, arrow_blocksize, overwrite=True
+    X_sdr = cholesky_solve_block_tridiagonal_arrowhead(
+        L_diagonal_blocks,
+        L_lower_diagonal_blocks,
+        L_arrow_bottom_blocks,
+        L_arrow_tip_block,
+        B,
     )
-    ax[1].set_title("X_sdr: Selected cholesky solver")
+
+    fig, ax = plt.subplots(1, 3)
+    ax[0].set_title("X_ref")
+    ax[0].matshow(X_ref)
+
+    ax[1].set_title("X_sdr")
     ax[1].matshow(X_sdr)
 
     X_diff = X_ref - X_sdr
