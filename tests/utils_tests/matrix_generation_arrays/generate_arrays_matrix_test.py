94fda745058b577e7a7dcc37384a9ba613a48bbc
vincent-maillou <vmaillou@iis.ee.ethz.ch>
2024-04-17 14:29:32 +0200

    added symmetric and seed tests

--- tests/utils_tests/matrix_generation_arrays/generate_arrays_matrix_test.py
+++ tests/utils_tests/matrix_generation_arrays/generate_arrays_matrix_test.py
@@ -1,4 +1,5 @@
 import numpy as np
+import pytest
 
 from sdr.utils.matrix_generation_arrays import (
     generate_block_tridiagonal_arrays,
@@ -15,13 +16,37 @@ DIAG_BLOCKSIZE = 4
 ARROWHEAD_BLOCKSIZE = 2
 
 
-def test_generate_block_tridiagonal_arrays():
+@pytest.mark.parametrize(
+    "seed",
+    [
+        None,
+        63,
+    ],
+)
+@pytest.mark.parametrize(
+    "symmetric",
+    [
+        False,
+        True,
+    ],
+)
+def test_generate_block_tridiagonal_arrays(
+    seed,
+    symmetric,
+):
     A_diagonal_blocks, A_lower_diagonal_blocks, A_upper_diagonal_blocks = (
-        generate_block_tridiagonal_arrays(N_DIAG_BLOCKS, DIAG_BLOCKSIZE)
+        generate_block_tridiagonal_arrays(
+            N_DIAG_BLOCKS,
+            DIAG_BLOCKSIZE,
+            symmetric=symmetric,
+            seed=seed,
+        )
     )
 
     A_dense_tridiag = convert_block_tridiagonal_arrays_to_dense(
-        A_diagonal_blocks, A_lower_diagonal_blocks, A_upper_diagonal_blocks
+        A_diagonal_blocks,
+        A_lower_diagonal_blocks,
+        A_upper_diagonal_blocks,
     )
 
     nonzero_indices = np.nonzero(A_dense_tridiag)
@@ -29,9 +54,28 @@ def test_generate_block_tridiagonal_arrays():
         cond_1 = j <= i + 2 * DIAG_BLOCKSIZE
         cond_2 = j >= i - 2 * DIAG_BLOCKSIZE
         assert cond_1 and cond_2
+        if symmetric:
+            assert A_dense_tridiag[i, j] == A_dense_tridiag[j, i]
 
 
-def test_generate_block_tridiagonal_arrowhead_arrays():
+@pytest.mark.parametrize(
+    "seed",
+    [
+        None,
+        63,
+    ],
+)
+@pytest.mark.parametrize(
+    "symmetric",
+    [
+        False,
+        True,
+    ],
+)
+def test_generate_block_tridiagonal_arrowhead_arrays(
+    seed,
+    symmetric,
+):
     matrix_size = N_DIAG_BLOCKS * DIAG_BLOCKSIZE + ARROWHEAD_BLOCKSIZE
 
     (
@@ -42,7 +86,11 @@ def test_generate_block_tridiagonal_arrowhead_arrays():
         A_arrow_right_blocks,
         A_arrow_tip_block,
     ) = generate_block_tridiagonal_arrowhead_arrays(
-        N_DIAG_BLOCKS + 1, DIAG_BLOCKSIZE, ARROWHEAD_BLOCKSIZE
+        N_DIAG_BLOCKS + 1,
+        DIAG_BLOCKSIZE,
+        ARROWHEAD_BLOCKSIZE,
+        symmetric=symmetric,
+        seed=seed,
     )
 
     A_dense_tridiag_arrowhead = convert_block_tridiagonal_arrowhead_arrays_to_dense(
@@ -61,3 +109,5 @@ def test_generate_block_tridiagonal_arrowhead_arrays():
         cond_3 = j >= matrix_size - ARROWHEAD_BLOCKSIZE
         cond_4 = i >= matrix_size - ARROWHEAD_BLOCKSIZE
         assert cond_1 and cond_2 or cond_3 or cond_4
+        if symmetric:
+            assert A_dense_tridiag_arrowhead[i, j] == A_dense_tridiag_arrowhead[j, i]
