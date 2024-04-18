bab9270bbab73f1503182644655a1364f5546a87
vincent-maillou <vmaillou@iis.ee.ethz.ch>
2024-04-18 12:26:38 +0200

    continued backbone

--- src/sdr/lu_dist/lu_dist_block_tridiagonal.py
+++ src/sdr/lu_dist/lu_dist_block_tridiagonal.py
@@ -37,61 +37,256 @@ def lu_dist_block_tridiagonal(
     comm_rank = comm.Get_rank()
 
     if comm_rank == 0:
-        () = top_factorize()
-        () = create_reduced_system()
+        (
+            L_diagonal_blocks_inv,
+            L_lower_diagonal_blocks,
+            U_diagonal_blocks,
+            U_upper_diagonal_blocks,
+        ) = top_factorize(
+            A_diagonal_blocks_local,
+            A_lower_diagonal_blocks_local,
+            A_upper_diagonal_blocks_local,
+        )
+
+        (
+            A_rs_diagonal_blocks,
+            A_rs_lower_diagonal_blocks,
+            A_rs_upper_diagonal_blocks,
+        ) = create_reduced_system(
+            A_diagonal_blocks_local,
+            A_bridges_lower,
+            A_bridges_upper,
+        )
     elif comm_rank == comm.Get_size() - 1:
-        () = bottom_factorize()
-        () = create_reduced_system()
+        (
+            L_diagonal_blocks_inv,
+            L_lower_diagonal_blocks,
+            U_diagonal_blocks,
+            U_upper_diagonal_blocks,
+        ) = bottom_factorize(
+            A_diagonal_blocks_local,
+            A_lower_diagonal_blocks_local,
+            A_upper_diagonal_blocks_local,
+        )
+
+        (
+            A_rs_diagonal_blocks,
+            A_rs_lower_diagonal_blocks,
+            A_rs_upper_diagonal_blocks,
+        ) = create_reduced_system(
+            A_diagonal_blocks_local,
+            A_bridges_lower,
+            A_bridges_upper,
+        )
     else:
-        () = middle_factorize()
-        () = create_reduced_system()
+        # Arrays that store the update of the 2sided pattern for the middle processes
+        A_top_2sided_arrow_blocks_local = np.empty(
+            (diag_blocksize, n_diag_blocks_partition * diag_blocksize),
+            dtype=A_diagonal_blocks_local.dtype,
+        )
+        A_left_2sided_arrow_blocks_local = np.empty(
+            (n_diag_blocks_partition * diag_blocksize, diag_blocksize),
+            dtype=A_diagonal_blocks_local.dtype,
+        )
 
-    () = inverse_reduced_system()
+        (
+            L_diagonal_blocks_inv,
+            L_lower_diagonal_blocks,
+            L_upper_2sided_arrow_blocks,
+            U_diagonal_blocks,
+            U_upper_diagonal_blocks,
+            U_left_2sided_arrow_blocks,
+        ) = middle_factorize(
+            A_diagonal_blocks_local,
+            A_lower_diagonal_blocks_local,
+            A_upper_diagonal_blocks_local,
+            A_top_2sided_arrow_blocks_local,
+            A_left_2sided_arrow_blocks_local,
+        )
 
-    () = update_sinv_reduced_system()
+        (
+            A_rs_diagonal_blocks,
+            A_rs_lower_diagonal_blocks,
+            A_rs_upper_diagonal_blocks,
+        ) = create_reduced_system(
+            A_diagonal_blocks_local,
+            A_bridges_lower,
+            A_bridges_upper,
+            A_top_2sided_arrow_blocks_local,
+            A_left_2sided_arrow_blocks_local,
+        )
+
+    (
+        X_rs_diagonal_blocks,
+        X_rs_lower_diagonal_blocks,
+        X_rs_upper_diagonal_blocks,
+    ) = inverse_reduced_system(
+        A_rs_diagonal_blocks,
+        A_rs_lower_diagonal_blocks,
+        A_rs_upper_diagonal_blocks,
+    )
+
+    (
+        X_diagonal_blocks_local,
+        X_lower_diagonal_blocks_local,
+        X_upper_diagonal_blocks_local,
+        X_top_2sided_arrow_blocks_local,
+        X_left_2sided_arrow_blocks_local,
+        X_bridges_lower,
+        X_bridges_upper,
+    ) = update_sinv_reduced_system(
+        X_rs_diagonal_blocks,
+        X_rs_lower_diagonal_blocks,
+        X_rs_upper_diagonal_blocks,
+        n_diag_blocks_partition,
+        diag_blocksize,
+    )
 
     if comm_rank == 0:
-        () = top_sinv()
+        (
+            X_diagonal_blocks_local,
+            X_lower_diagonal_blocks_local,
+            X_upper_diagonal_blocks_local,
+        ) = top_sinv(
+            X_diagonal_blocks_local,
+            X_lower_diagonal_blocks_local,
+            X_upper_diagonal_blocks_local,
+            L_diagonal_blocks_inv,
+            L_lower_diagonal_blocks,
+            U_diagonal_blocks,
+            U_upper_diagonal_blocks,
+        )
     elif comm_rank == comm.Get_size() - 1:
-        () = bottom_sinv()
+        (
+            X_diagonal_blocks_local,
+            X_lower_diagonal_blocks_local,
+            X_upper_diagonal_blocks_local,
+        ) = bottom_sinv(
+            X_diagonal_blocks_local,
+            X_lower_diagonal_blocks_local,
+            X_upper_diagonal_blocks_local,
+            L_diagonal_blocks_inv,
+            L_lower_diagonal_blocks,
+            U_diagonal_blocks,
+            U_upper_diagonal_blocks,
+        )
     else:
-        () = middle_sinv()
+        (
+            X_diagonal_blocks_local,
+            X_lower_diagonal_blocks_local,
+            X_upper_diagonal_blocks_local,
+        ) = middle_sinv(
+            X_diagonal_blocks_local,
+            X_lower_diagonal_blocks_local,
+            X_upper_diagonal_blocks_local,
+            X_top_2sided_arrow_blocks_local,
+            X_left_2sided_arrow_blocks_local,
+            L_diagonal_blocks_inv,
+            L_lower_diagonal_blocks,
+            L_upper_2sided_arrow_blocks,
+            U_diagonal_blocks,
+            U_upper_diagonal_blocks,
+            U_left_2sided_arrow_blocks,
+        )
 
+    return (
+        X_diagonal_blocks_local,
+        X_lower_diagonal_blocks_local,
+        X_upper_diagonal_blocks_local,
+        X_bridges_lower,
+        X_bridges_upper,
+    )
+
+
+def top_factorize(
+    A_diagonal_blocks_local,
+    A_lower_diagonal_blocks_local,
+    A_upper_diagonal_blocks_local,
+):
     return ()
 
 
-def top_factorize():
+def bottom_factorize(
+    A_diagonal_blocks_local,
+    A_lower_diagonal_blocks_local,
+    A_upper_diagonal_blocks_local,
+):
     return ()
 
 
-def bottom_factorize():
+def middle_factorize(
+    A_diagonal_blocks_local,
+    A_lower_diagonal_blocks_local,
+    A_upper_diagonal_blocks_local,
+    A_top_2sided_arrow_blocks_local,
+    A_left_2sided_arrow_blocks_local,
+):
     return ()
 
 
-def middle_factorize():
+def create_reduced_system(
+    A_diagonal_blocks_local,
+    A_bridges_lower,
+    A_bridges_upper,
+    A_top_2sided_arrow_blocks_local=None,
+    A_left_2sided_arrow_blocks_local=None,
+):
     return ()
 
 
-def create_reduced_system():
+def inverse_reduced_system(
+    A_rs_diagonal_blocks,
+    A_rs_lower_diagonal_blocks,
+    A_rs_upper_diagonal_blocks,
+):
     return ()
 
 
-def inverse_reduced_system():
+def update_sinv_reduced_system(
+    X_rs_diagonal_blocks,
+    X_rs_lower_diagonal_blocks,
+    X_rs_upper_diagonal_blocks,
+    n_diag_blocks_partition,
+    diag_blocksize,
+):
     return ()
 
 
-def update_sinv_reduced_system():
+def top_sinv(
+    X_diagonal_blocks_local,
+    X_lower_diagonal_blocks_local,
+    X_upper_diagonal_blocks_local,
+    L_diagonal_blocks_inv,
+    L_lower_diagonal_blocks,
+    U_diagonal_blocks,
+    U_upper_diagonal_blocks,
+):
     return ()
 
 
-def top_sinv():
+def bottom_sinv(
+    X_diagonal_blocks_local,
+    X_lower_diagonal_blocks_local,
+    X_upper_diagonal_blocks_local,
+    L_diagonal_blocks_inv,
+    L_lower_diagonal_blocks,
+    U_diagonal_blocks,
+    U_upper_diagonal_blocks,
+):
     return ()
 
 
-def bottom_sinv():
+def middle_sinv(
+    X_diagonal_blocks_local,
+    X_lower_diagonal_blocks_local,
+    X_upper_diagonal_blocks_local,
+    X_top_2sided_arrow_blocks_local,
+    X_left_2sided_arrow_blocks_local,
+    L_diagonal_blocks_inv,
+    L_lower_diagonal_blocks,
+    L_upper_2sided_arrow_blocks,
+    U_diagonal_blocks,
+    U_upper_diagonal_blocks,
+    U_left_2sided_arrow_blocks,
+):
     return ()
-
-
-def middle_sinv():
-    return ()
-
