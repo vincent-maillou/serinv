7f31db7c51aeafddad11ae928db94cb25102d327
vincent-maillou <vmaillou@iis.ee.ethz.ch>
2024-05-31 12:08:41 +0200

    added pobtasi + gpu + tests

--- src/serinv/sequential/pobtasi.py
+++ src/serinv/sequential/pobtasi.py
@@ -1,7 +1,16 @@
 # Copyright 2023-2024 ETH Zurich. All rights reserved.
 
+try:
+    import cupy as cp
+    import cupyx.scipy.linalg as cu_la
+
+    CUPY_AVAIL = True
+
+except:
+    CUPY_AVAIL = False
+
 import numpy as np
-import scipy.linalg as la
+import scipy.linalg as np_la
 
 
 def pobtasi(
@@ -10,8 +19,12 @@ def pobtasi(
     L_arrow_bottom_blocks: np.ndarray,
     L_arrow_tip_block: np.ndarray,
 ) -> tuple[np.ndarray, np.ndarray, np.ndarray, np.ndarray]:
-    """Perform a selected inversion of a block tridiagonal matrix using a
-    sequential algorithm on CPU backend.
+    """Perform a selected inversion of a block tridiagonal arrowhead matrix using a
+    sequential block algorithm.
+
+    Note:
+    -----
+    - If a device array is given, the algorithm will run on the GPU.
 
     Parameters
     ----------
@@ -36,87 +49,80 @@ def pobtasi(
         Arrow tip block of the selected inverse.
     """
 
-    diag_blocksize = L_diagonal_blocks.shape[0]
+    la = np_la
+    if CUPY_AVAIL:
+        xp = cp.get_array_module(L_diagonal_blocks)
+        if xp == cp:
+            la = cu_la
+    else:
+        xp = np
+
+    diag_blocksize = L_diagonal_blocks.shape[1]
     arrow_blocksize = L_arrow_tip_block.shape[0]
-    n_diag_blocks = L_diagonal_blocks.shape[1] // diag_blocksize
+    n_diag_blocks = L_diagonal_blocks.shape[0]
 
-    X_diagonal_blocks = np.zeros_like(L_diagonal_blocks)
-    X_lower_diagonal_blocks = np.zeros_like(L_lower_diagonal_blocks)
-    X_arrow_bottom_blocks = np.zeros_like(L_arrow_bottom_blocks)
-    X_arrow_tip_block = np.zeros_like(L_arrow_tip_block)
+    X_diagonal_blocks = xp.zeros_like(L_diagonal_blocks)
+    X_lower_diagonal_blocks = xp.zeros_like(L_lower_diagonal_blocks)
+    X_arrow_bottom_blocks = xp.zeros_like(L_arrow_bottom_blocks)
+    X_arrow_tip_block = xp.zeros_like(L_arrow_tip_block)
+    L_last_blk_inv = xp.zeros_like(L_arrow_tip_block)
 
-    L_last_blk_inv = np.zeros(
-        (arrow_blocksize, arrow_blocksize), dtype=L_diagonal_blocks.dtype
-    )
-    L_blk_inv = np.zeros(
+    L_blk_inv = xp.zeros(
         (diag_blocksize, diag_blocksize), dtype=L_diagonal_blocks.dtype
     )
 
     L_last_blk_inv = la.solve_triangular(
-        L_arrow_tip_block[:, :], np.eye(arrow_blocksize), lower=True
+        L_arrow_tip_block[:, :], xp.eye(arrow_blocksize), lower=True
     )
 
-    X_arrow_tip_block[:, :] = L_last_blk_inv.T @ L_last_blk_inv
+    X_arrow_tip_block[:, :] = L_last_blk_inv.conj().T @ L_last_blk_inv
 
     L_blk_inv = la.solve_triangular(
-        L_diagonal_blocks[:, -diag_blocksize:],
-        np.eye(diag_blocksize),
+        L_diagonal_blocks[-1, :, :],
+        xp.eye(diag_blocksize),
         lower=True,
     )
 
     # X_{ndb+1, ndb} = -X_{ndb+1, ndb+1} L_{ndb+1, ndb} L_{ndb, ndb}^{-1}
-    X_arrow_bottom_blocks[:, -diag_blocksize:] = (
-        -X_arrow_tip_block[:, :]
-        @ L_arrow_bottom_blocks[:, -diag_blocksize:]
-        @ L_blk_inv
+    X_arrow_bottom_blocks[-1, :, :] = (
+        -X_arrow_tip_block[:, :] @ L_arrow_bottom_blocks[-1, :, :] @ L_blk_inv
     )
 
     # X_{ndb, ndb} = (L_{ndb, ndb}^{-T} - X_{ndb+1, ndb}^{T} L_{ndb+1, ndb}) L_{ndb, ndb}^{-1}
-    X_diagonal_blocks[:, -diag_blocksize:] = (
-        L_blk_inv.T
-        - X_arrow_bottom_blocks[:, -diag_blocksize:].T
-        @ L_arrow_bottom_blocks[:, -diag_blocksize:]
+    X_diagonal_blocks[-1, :, :] = (
+        L_blk_inv.conj().T
+        - X_arrow_bottom_blocks[-1, :, :].conj().T @ L_arrow_bottom_blocks[-1, :, :]
     ) @ L_blk_inv
 
     for i in range(n_diag_blocks - 2, -1, -1):
         L_blk_inv = la.solve_triangular(
-            L_diagonal_blocks[:, i * diag_blocksize : (i + 1) * diag_blocksize],
-            np.eye(diag_blocksize),
+            L_diagonal_blocks[i, :, :],
+            xp.eye(diag_blocksize),
             lower=True,
         )
 
         # --- Off-diagonal block part ---
         # X_{i+1, i} = (-X_{i+1, i+1} L_{i+1, i} - X_{ndb+1, i+1}^{T} L_{ndb+1, i}) L_{i, i}^{-1}
-        X_lower_diagonal_blocks[:, i * diag_blocksize : (i + 1) * diag_blocksize] = (
-            -X_diagonal_blocks[:, (i + 1) * diag_blocksize : (i + 2) * diag_blocksize]
-            @ L_lower_diagonal_blocks[:, i * diag_blocksize : (i + 1) * diag_blocksize]
-            - X_arrow_bottom_blocks[
-                :, (i + 1) * diag_blocksize : (i + 2) * diag_blocksize
-            ].T
-            @ L_arrow_bottom_blocks[:, i * diag_blocksize : (i + 1) * diag_blocksize]
+        X_lower_diagonal_blocks[i, :, :] = (
+            -X_diagonal_blocks[i + 1, :, :] @ L_lower_diagonal_blocks[i, :, :]
+            - X_arrow_bottom_blocks[i + 1, :, :].conj().T
+            @ L_arrow_bottom_blocks[i, :, :]
         ) @ L_blk_inv
 
         # --- Arrowhead part ---
         # X_{ndb+1, i} = (- X_{ndb+1, i+1} L_{i+1, i} - X_{ndb+1, ndb+1} L_{ndb+1, i}) L_{i, i}^{-1}
-        X_arrow_bottom_blocks[:, i * diag_blocksize : (i + 1) * diag_blocksize] = (
-            -X_arrow_bottom_blocks[
-                :, (i + 1) * diag_blocksize : (i + 2) * diag_blocksize
-            ]
-            @ L_lower_diagonal_blocks[:, i * diag_blocksize : (i + 1) * diag_blocksize]
-            - X_arrow_tip_block[:, :]
-            @ L_arrow_bottom_blocks[:, i * diag_blocksize : (i + 1) * diag_blocksize]
+        X_arrow_bottom_blocks[i, :, :] = (
+            -X_arrow_bottom_blocks[i + 1, :, :] @ L_lower_diagonal_blocks[i, :, :]
+            - X_arrow_tip_block[:, :] @ L_arrow_bottom_blocks[i, :, :]
         ) @ L_blk_inv
 
         # --- Diagonal block part ---
-        # X_{i, i} = (L_{i, i}^{-T} - X_{i+1, i}^{T} L_{i+1, i} - X_{ndb+1, i}.T L_{ndb+1, i}) L_{i, i}^{-1}
-        X_diagonal_blocks[:, i * diag_blocksize : (i + 1) * diag_blocksize] = (
-            L_blk_inv.T
-            - X_lower_diagonal_blocks[
-                :, i * diag_blocksize : (i + 1) * diag_blocksize
-            ].T
-            @ L_lower_diagonal_blocks[:, i * diag_blocksize : (i + 1) * diag_blocksize]
-            - X_arrow_bottom_blocks[:, i * diag_blocksize : (i + 1) * diag_blocksize].T
-            @ L_arrow_bottom_blocks[:, i * diag_blocksize : (i + 1) * diag_blocksize]
+        # X_{i, i} = (L_{i, i}^{-T} - X_{i+1, i}^{T} L_{i+1, i} - X_{ndb+1, i}.conj().T L_{ndb+1, i}) L_{i, i}^{-1}
+        X_diagonal_blocks[i, :, :] = (
+            L_blk_inv.conj().T
+            - X_lower_diagonal_blocks[i, :, :].conj().T
+            @ L_lower_diagonal_blocks[i, :, :]
+            - X_arrow_bottom_blocks[i, :, :].conj().T @ L_arrow_bottom_blocks[i, :, :]
         ) @ L_blk_inv
 
     return (
--- tests/sequential/cpu/test_pobtasi.py
+++ tests/sequential/cpu/test_pobtasi.py
@@ -2,7 +2,6 @@
 
 try:
     import cupy as cp
-    import cupyx.scipy.linalg as cu_la
 
     CUPY_AVAIL = True
 
@@ -10,40 +9,39 @@ except:
     CUPY_AVAIL = False
 
 import numpy as np
-import scipy.linalg as np_la
 
 import pytest
 
-from serinv.sequential import ddbtaf, ddbtasi
+from serinv.sequential import pobtaf, pobtasi
 
 
 @pytest.mark.parametrize("diagonal_blocksize", [2, 3])
 @pytest.mark.parametrize("arrowhead_blocksize", [2, 3])
 @pytest.mark.parametrize("n_diag_blocks", [1, 2, 3])
 @pytest.mark.parametrize("device_array", [False, True])
-def test_scddbtaf(
+def test_pobtasi(
     dd_bta,
     bta_dense_to_arrays,
+    bta_symmetrize,
     diagonal_blocksize,
     arrowhead_blocksize,
     n_diag_blocks,
 ):
-    la = np_la
     if CUPY_AVAIL:
         xp = cp.get_array_module(dd_bta)
-        if xp == cp:
-            la = cu_la
     else:
         xp = np
 
-    X_ref = np.linalg.inv(dd_bta)
+    A = bta_symmetrize(dd_bta)
+
+    X_ref = xp.linalg.inv(A)
 
     (
         X_diagonal_blocks_ref,
         X_lower_diagonal_blocks_ref,
-        X_upper_diagonal_blocks_ref,
+        _,
         X_arrow_bottom_blocks_ref,
-        X_arrow_right_blocks_ref,
+        _,
         X_arrow_tip_block_ref,
     ) = bta_dense_to_arrays(
         X_ref, diagonal_blocksize, arrowhead_blocksize, n_diag_blocks
@@ -52,53 +50,37 @@ def test_scddbtaf(
     (
         A_diagonal_blocks,
         A_lower_diagonal_blocks,
-        A_upper_diagonal_blocks,
+        _,
         A_arrow_bottom_blocks,
-        A_arrow_right_blocks,
+        _,
         A_arrow_tip_block,
-    ) = bta_dense_to_arrays(
-        dd_bta, diagonal_blocksize, arrowhead_blocksize, n_diag_blocks
-    )
+    ) = bta_dense_to_arrays(A, diagonal_blocksize, arrowhead_blocksize, n_diag_blocks)
 
     (
         L_diagonal_blocks,
         L_lower_diagonal_blocks,
         L_arrow_bottom_blocks,
         L_arrow_tip_block,
-        U_diagonal_blocks,
-        U_upper_diagonal_blocks,
-        U_arrow_right_blocks,
-        U_arrow_tip_block,
-    ) = ddbtaf(
+    ) = pobtaf(
         A_diagonal_blocks,
         A_lower_diagonal_blocks,
-        A_upper_diagonal_blocks,
         A_arrow_bottom_blocks,
-        A_arrow_right_blocks,
         A_arrow_tip_block,
     )
 
     (
         X_diagonal_blocks_serinv,
         X_lower_diagonal_blocks_serinv,
-        X_upper_diagonal_blocks_serinv,
         X_arrow_bottom_blocks_serinv,
-        X_arrow_right_blocks_serinv,
         X_arrow_tip_block_serinv,
-    ) = ddbtasi(
+    ) = pobtasi(
         L_diagonal_blocks,
         L_lower_diagonal_blocks,
         L_arrow_bottom_blocks,
         L_arrow_tip_block,
-        U_diagonal_blocks,
-        U_upper_diagonal_blocks,
-        U_arrow_right_blocks,
-        U_arrow_tip_block,
     )
 
-    assert np.allclose(X_diagonal_blocks_ref, X_diagonal_blocks_serinv)
-    assert np.allclose(X_lower_diagonal_blocks_ref, X_lower_diagonal_blocks_serinv)
-    assert np.allclose(X_upper_diagonal_blocks_ref, X_upper_diagonal_blocks_serinv)
-    assert np.allclose(X_arrow_bottom_blocks_ref, X_arrow_bottom_blocks_serinv)
-    assert np.allclose(X_arrow_right_blocks_ref, X_arrow_right_blocks_serinv)
-    assert np.allclose(X_arrow_tip_block_ref, X_arrow_tip_block_serinv)
+    assert xp.allclose(X_diagonal_blocks_ref, X_diagonal_blocks_serinv)
+    assert xp.allclose(X_lower_diagonal_blocks_ref, X_lower_diagonal_blocks_serinv)
+    assert xp.allclose(X_arrow_bottom_blocks_ref, X_arrow_bottom_blocks_serinv)
+    assert xp.allclose(X_arrow_tip_block_ref, X_arrow_tip_block_serinv)
