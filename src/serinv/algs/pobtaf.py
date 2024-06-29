ea22f3e64158295f778374fd7e5c2d50537bda75
vincent-maillou <vmaillou@iis.ee.ethz.ch>
2024-06-29 13:29:54 +0200

    in-place + streaming

--- src/serinv/algs/pobtaf.py
+++ src/serinv/algs/pobtaf.py
@@ -11,18 +11,20 @@ except:
 
 import numpy as np
 import scipy.linalg as np_la
+from numpy.typing import ArrayLike
 
 
 def pobtaf(
-    A_diagonal_blocks: np.ndarray | cp.ndarray,
-    A_lower_diagonal_blocks: np.ndarray | cp.ndarray,
-    A_arrow_bottom_blocks: np.ndarray | cp.ndarray,
-    A_arrow_tip_block: np.ndarray | cp.ndarray,
+    A_diagonal_blocks: ArrayLike,
+    A_lower_diagonal_blocks: ArrayLike,
+    A_arrow_bottom_blocks: ArrayLike,
+    A_arrow_tip_block: ArrayLike,
+    device_streaming: bool = False,
 ) -> tuple[
-    np.ndarray | cp.ndarray,
-    np.ndarray | cp.ndarray,
-    np.ndarray | cp.ndarray,
-    np.ndarray | cp.ndarray,
+    ArrayLike,
+    ArrayLike,
+    ArrayLike,
+    ArrayLike,
 ]:
     """Perform the Cholesky factorization of a block tridiagonal arrowhead matrix using
     a sequential block algorithm.
@@ -35,23 +37,52 @@ def pobtaf(
 
     Parameters
     ----------
-    A_diagonal_blocks : np.ndarray | cp.ndarray
+    A_diagonal_blocks : ArrayLike
         Diagonal blocks of the matrix.
-    A_lower_diagonal_blocks : np.ndarray | cp.ndarray
+    A_lower_diagonal_blocks : ArrayLike
         Lower diagonal blocks of the matrix.
-    A_arrow_bottom_blocks : np.ndarray | cp.ndarray
+    A_arrow_bottom_blocks : ArrayLike
         Arrow bottom blocks of the matrix.
-    A_arrow_tip_block : np.ndarray | cp.ndarray
+    A_arrow_tip_block : ArrayLike
         Arrow tip block of the matrix.
+    device_streaming : bool
+        Whether to use streamed GPU computation.
 
     Returns
     -------
-    L_diagonal_blocks : np.ndarray | cp.ndarray
-    L_lower_diagonal_blocks : np.ndarray | cp.ndarray
-    L_arrow_bottom_blocks : np.ndarray | cp.ndarray
-    L_arrow_tip_block : np.ndarray | cp.ndarray
+    L_diagonal_blocks : ArrayLike
+    L_lower_diagonal_blocks : ArrayLike
+    L_arrow_bottom_blocks : ArrayLike
+    L_arrow_tip_block : ArrayLike
     """
 
+    if CUPY_AVAIL and cp.get_array_module(A_diagonal_blocks) == np and device_streaming:
+        return _streaming_pobtaf(
+            A_diagonal_blocks,
+            A_lower_diagonal_blocks,
+            A_arrow_bottom_blocks,
+            A_arrow_tip_block,
+        )
+
+    return _pobtaf(
+        A_diagonal_blocks,
+        A_lower_diagonal_blocks,
+        A_arrow_bottom_blocks,
+        A_arrow_tip_block,
+    )
+
+
+def _pobtaf(
+    A_diagonal_blocks: ArrayLike,
+    A_lower_diagonal_blocks: ArrayLike,
+    A_arrow_bottom_blocks: ArrayLike,
+    A_arrow_tip_block: ArrayLike,
+) -> tuple[
+    ArrayLike,
+    ArrayLike,
+    ArrayLike,
+    ArrayLike,
+]:
     la = np_la
     if CUPY_AVAIL:
         xp = cp.get_array_module(A_diagonal_blocks)
@@ -63,10 +94,10 @@ def pobtaf(
     diag_blocksize = A_diagonal_blocks.shape[1]
     n_diag_blocks = A_diagonal_blocks.shape[0]
 
-    L_diagonal_blocks = xp.zeros_like(A_diagonal_blocks)
-    L_lower_diagonal_blocks = xp.zeros_like(A_lower_diagonal_blocks)
-    L_arrow_bottom_blocks = xp.zeros_like(A_arrow_bottom_blocks)
-    L_arrow_tip_block = xp.zeros_like(A_arrow_tip_block)
+    L_diagonal_blocks = A_diagonal_blocks
+    L_lower_diagonal_blocks = A_lower_diagonal_blocks
+    L_arrow_bottom_blocks = A_arrow_bottom_blocks
+    L_arrow_tip_block = A_arrow_tip_block
 
     L_inv_temp = xp.zeros(
         (diag_blocksize, diag_blocksize), dtype=A_diagonal_blocks.dtype
@@ -136,6 +167,159 @@ def pobtaf(
 
     # L_{ndb+1, ndb+1} = chol(A_{ndb+1, ndb+1})
     L_arrow_tip_block[:, :] = xp.linalg.cholesky(A_arrow_tip_block[:, :])
+
+    return (
+        L_diagonal_blocks,
+        L_lower_diagonal_blocks,
+        L_arrow_bottom_blocks,
+        L_arrow_tip_block,
+    )
+
+
+def _streaming_pobtaf(
+    A_diagonal_blocks: ArrayLike,
+    A_lower_diagonal_blocks: ArrayLike,
+    A_arrow_bottom_blocks: ArrayLike,
+    A_arrow_tip_block: ArrayLike,
+) -> tuple[
+    ArrayLike,
+    ArrayLike,
+    ArrayLike,
+    ArrayLike,
+]:
+    cp.cuda.nvtx.RangePush("_streaming_pobtaf:mem_init")
+    diag_blocksize = A_diagonal_blocks.shape[1]
+
+    # X hosts arrays pointers
+    L_diagonal_blocks = A_diagonal_blocks
+    L_lower_diagonal_blocks = A_lower_diagonal_blocks
+    L_arrow_bottom_blocks = A_arrow_bottom_blocks
+    L_arrow_tip_block = A_arrow_tip_block
+
+    # Device buffers
+    A_diagonal_blocks_d = cp.zeros(
+        (2, *A_diagonal_blocks.shape[1:]), dtype=A_diagonal_blocks.dtype
+    )
+    A_lower_diagonal_blocks_d = cp.zeros_like(A_diagonal_blocks[0])
+    A_arrow_bottom_blocks_d = cp.zeros(
+        (2, *A_arrow_bottom_blocks.shape[1:]), dtype=A_arrow_bottom_blocks.dtype
+    )
+    A_arrow_tip_block_d = cp.zeros_like(A_arrow_tip_block)
+    A_arrow_tip_block_d.set(arr=A_arrow_tip_block)
+
+    # X Device buffers arrays pointers
+    L_diagonal_blocks_d = A_diagonal_blocks_d
+    L_lower_diagonal_blocks_d = A_lower_diagonal_blocks_d
+    L_arrow_bottom_blocks_d = A_arrow_bottom_blocks_d
+    L_arrow_tip_block_d = A_arrow_tip_block_d
+
+    # Buffers for the intermediate results of the forward cholesky
+    L_inv_temp_d = cp.zeros_like(A_diagonal_blocks[0])
+    cp.cuda.nvtx.RangePop()
+
+    # Forward pass
+    cp.cuda.nvtx.RangePush("_streaming_pobtaf:fwd_cholesky")
+
+    A_diagonal_blocks_d[0, :, :].set(arr=A_diagonal_blocks[0, :, :])
+    A_arrow_bottom_blocks_d[0, :, :].set(arr=A_arrow_bottom_blocks[0, :, :])
+
+    n_diag_blocks = A_diagonal_blocks.shape[0]
+    for i in range(0, n_diag_blocks - 1):
+        # --- Host 2 Device transfers ---
+        A_diagonal_blocks_d[(i + 1) % 2, :, :].set(arr=A_diagonal_blocks[i + 1, :, :])
+        A_lower_diagonal_blocks_d[:, :].set(arr=A_lower_diagonal_blocks[i, :, :])
+        A_arrow_bottom_blocks_d[(i + 1) % 2, :, :].set(
+            arr=A_arrow_bottom_blocks[i + 1, :, :]
+        )
+
+        # --- Computations ---
+        # L_{i, i} = chol(A_{i, i})
+        L_diagonal_blocks_d[i % 2, :, :] = cp.linalg.cholesky(
+            A_diagonal_blocks_d[i % 2, :, :]
+        )
+
+        # Temporary storage of used twice lower triangular solving
+        L_inv_temp_d[:, :] = (
+            cu_la.solve_triangular(
+                L_diagonal_blocks_d[i % 2, :, :],
+                cp.eye(diag_blocksize),
+                lower=True,
+            )
+            .conj()
+            .T
+        )
+
+        # L_{i+1, i} = A_{i+1, i} @ L_{i, i}^{-T}
+        L_lower_diagonal_blocks_d[:, :] = A_lower_diagonal_blocks_d[:, :] @ L_inv_temp_d
+
+        # L_{ndb+1, i} = A_{ndb+1, i} @ L_{i, i}^{-T}
+        L_arrow_bottom_blocks_d[i % 2, :, :] = (
+            A_arrow_bottom_blocks_d[i % 2, :, :] @ L_inv_temp_d
+        )
+
+        # Update next diagonal block
+        # A_{i+1, i+1} = A_{i+1, i+1} - L_{i+1, i} @ L_{i+1, i}.conj().T
+        A_diagonal_blocks_d[(i + 1) % 2, :, :] = (
+            A_diagonal_blocks_d[(i + 1) % 2, :, :]
+            - L_lower_diagonal_blocks_d[:, :] @ L_lower_diagonal_blocks_d[:, :].conj().T
+        )
+
+        # A_{ndb+1, i+1} = A_{ndb+1, i+1} - L_{ndb+1, i} @ L_{i+1, i}.conj().T
+        A_arrow_bottom_blocks_d[(i + 1) % 2, :, :] = (
+            A_arrow_bottom_blocks_d[(i + 1) % 2, :, :]
+            - L_arrow_bottom_blocks_d[i % 2, :, :]
+            @ L_lower_diagonal_blocks_d[:, :].conj().T
+        )
+
+        # A_{ndb+1, ndb+1} = A_{ndb+1, ndb+1} - L_{ndb+1, i} @ L_{ndb+1, i}.conj().T
+        A_arrow_tip_block_d[:, :] = (
+            A_arrow_tip_block_d[:, :]
+            - L_arrow_bottom_blocks_d[i % 2, :, :]
+            @ L_arrow_bottom_blocks_d[i % 2, :, :].conj().T
+        )
+
+        # --- Device 2 Host transfers ---
+        L_diagonal_blocks_d[i % 2, :, :].get(out=L_diagonal_blocks[i, :, :])
+        L_lower_diagonal_blocks_d[:, :].get(out=L_lower_diagonal_blocks[i, :, :])
+        L_arrow_bottom_blocks_d[i % 2, :, :].get(out=L_arrow_bottom_blocks[i, :, :])
+
+    # L_{ndb, ndb} = chol(A_{ndb, ndb})
+    L_diagonal_blocks_d[(n_diag_blocks - 1) % 2, :, :] = cp.linalg.cholesky(
+        A_diagonal_blocks_d[(n_diag_blocks - 1) % 2, :, :]
+    )
+
+    # L_{ndb+1, ndb} = A_{ndb+1, ndb} @ L_{ndb, ndb}^{-T}
+    L_arrow_bottom_blocks_d[(n_diag_blocks - 1) % 2, :, :] = (
+        A_arrow_bottom_blocks_d[(n_diag_blocks - 1) % 2, :, :]
+        @ cu_la.solve_triangular(
+            L_diagonal_blocks_d[(n_diag_blocks - 1) % 2, :, :],
+            cp.eye(diag_blocksize),
+            lower=True,
+        )
+        .conj()
+        .T
+    )
+
+    # A_{ndb+1, ndb+1} = A_{ndb+1, ndb+1} - L_{ndb+1, ndb} @ L_{ndb+1, ndb}^{T}
+    A_arrow_tip_block_d[:, :] = (
+        A_arrow_tip_block_d[:, :]
+        - L_arrow_bottom_blocks_d[(n_diag_blocks - 1) % 2, :, :]
+        @ L_arrow_bottom_blocks_d[(n_diag_blocks - 1) % 2, :, :].conj().T
+    )
+
+    # L_{ndb+1, ndb+1} = chol(A_{ndb+1, ndb+1})
+    L_arrow_tip_block_d[:, :] = cp.linalg.cholesky(A_arrow_tip_block_d[:, :])
+
+    L_diagonal_blocks_d[(n_diag_blocks - 1) % 2, :, :].get(
+        out=L_diagonal_blocks[-1, :, :]
+    )
+    L_arrow_bottom_blocks_d[(n_diag_blocks - 1) % 2, :, :].get(
+        out=L_arrow_bottom_blocks[-1, :, :]
+    )
+    L_arrow_tip_block_d[:, :].get(out=L_arrow_tip_block[:, :])
+
+    cp.cuda.Device().synchronize()
+    cp.cuda.nvtx.RangePop()
 
     return (
         L_diagonal_blocks,
--- tests/algs/test_pobtaf.py
+++ tests/algs/test_pobtaf.py
@@ -2,6 +2,7 @@
 
 try:
     import cupy as cp
+    import cupyx as cpx
 
     CUPY_AVAIL = True
 
@@ -20,14 +21,16 @@ from serinv.algs import pobtaf
 @pytest.mark.parametrize("n_diag_blocks", [1, 2, 3])
 @pytest.mark.parametrize("device_array", [False, True])
 @pytest.mark.parametrize("dtype", [np.float64, np.complex128])
+@pytest.mark.parametrize("device_streaming", [False, True])
 def test_pobtaf(
     dd_bta,
     bta_dense_to_arrays,
-    bta_arrays_to_dense,
     bta_symmetrize,
     diagonal_blocksize,
     arrowhead_blocksize,
     n_diag_blocks,
+    device_array,
+    device_streaming,
 ):
     if CUPY_AVAIL:
         xp = cp.get_array_module(dd_bta)
@@ -39,33 +42,73 @@ def test_pobtaf(
     (
         A_diagonal_blocks,
         A_lower_diagonal_blocks,
-        A_upper_diagonal_blocks,
+        _,
         A_arrow_bottom_blocks,
-        A_arrow_right_blocks,
+        _,
         A_arrow_tip_block,
     ) = bta_dense_to_arrays(
         bta_symmetrize(dd_bta), diagonal_blocksize, arrowhead_blocksize, n_diag_blocks
     )
 
+    if CUPY_AVAIL and device_streaming and not device_array:
+        A_diagonal_blocks_pinned = cpx.zeros_like_pinned(A_diagonal_blocks)
+        A_diagonal_blocks_pinned[:, :, :] = A_diagonal_blocks[:, :, :]
+        A_lower_diagonal_blocks_pinned = cpx.zeros_like_pinned(A_lower_diagonal_blocks)
+        A_lower_diagonal_blocks_pinned[:, :, :] = A_lower_diagonal_blocks[:, :, :]
+        A_arrow_bottom_blocks_pinned = cpx.zeros_like_pinned(A_arrow_bottom_blocks)
+        A_arrow_bottom_blocks_pinned[:, :, :] = A_arrow_bottom_blocks[:, :, :]
+        A_arrow_tip_block_pinned = cpx.zeros_like_pinned(A_arrow_tip_block)
+        A_arrow_tip_block_pinned[:, :] = A_arrow_tip_block[:, :]
+
+        A_diagonal_blocks = A_diagonal_blocks_pinned
+        A_lower_diagonal_blocks = A_lower_diagonal_blocks_pinned
+        A_arrow_bottom_blocks = A_arrow_bottom_blocks_pinned
+        A_arrow_tip_block = A_arrow_tip_block_pinned
+
     (
-        L_diagonal_blocks,
-        L_lower_diagonal_blocks,
-        L_arrow_bottom_blocks,
-        L_arrow_tip_block,
+        L_diagonal_blocks_serinv,
+        L_lower_diagonal_blocks_serinv,
+        L_arrow_bottom_blocks_serinv,
+        L_arrow_tip_block_serinv,
     ) = pobtaf(
         A_diagonal_blocks,
         A_lower_diagonal_blocks,
         A_arrow_bottom_blocks,
         A_arrow_tip_block,
+        device_streaming,
     )
 
-    L_serinv = bta_arrays_to_dense(
-        L_diagonal_blocks,
-        L_lower_diagonal_blocks,
-        xp.zeros_like(A_upper_diagonal_blocks),
-        L_arrow_bottom_blocks,
-        xp.zeros_like(A_arrow_right_blocks),
-        L_arrow_tip_block,
+    (
+        L_diagonal_blocks_ref,
+        L_lower_diagonal_blocks_ref,
+        _,
+        L_arrow_bottom_blocks_ref,
+        _,
+        L_arrow_tip_block_ref,
+    ) = bta_dense_to_arrays(
+        L_ref, diagonal_blocksize, arrowhead_blocksize, n_diag_blocks
     )
 
-    assert xp.allclose(L_ref, L_serinv)
+    # Check algorithm validity
+    assert np.allclose(L_diagonal_blocks_ref, L_diagonal_blocks_serinv)
+    assert np.allclose(L_lower_diagonal_blocks_ref, L_lower_diagonal_blocks_serinv)
+    assert np.allclose(L_arrow_bottom_blocks_ref, L_arrow_bottom_blocks_serinv)
+    assert np.allclose(L_arrow_tip_block_ref, L_arrow_tip_block_serinv)
+
+    # Check for in-place operations
+    if device_array:
+        assert L_diagonal_blocks_serinv.data == A_diagonal_blocks.data
+        assert L_lower_diagonal_blocks_serinv.data == A_lower_diagonal_blocks.data
+        assert L_arrow_bottom_blocks_serinv.data == A_arrow_bottom_blocks.data
+        assert L_arrow_tip_block_serinv.data == A_arrow_tip_block.data
+    else:
+        assert L_diagonal_blocks_serinv.ctypes.data == A_diagonal_blocks.ctypes.data
+        assert (
+            L_lower_diagonal_blocks_serinv.ctypes.data
+            == A_lower_diagonal_blocks.ctypes.data
+        )
+        assert (
+            L_arrow_bottom_blocks_serinv.ctypes.data
+            == A_arrow_bottom_blocks.ctypes.data
+        )
+        assert L_arrow_tip_block_serinv.ctypes.data == A_arrow_tip_block.ctypes.data
