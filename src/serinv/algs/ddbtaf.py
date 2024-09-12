27e25b1dbcdb080709519b2820f49609136cc206
vincent-maillou <vmaillou@iis.ee.ethz.ch>
2024-09-12 11:28:17 +0200

    modified to store in palce of A, also return the applied pivoting

--- src/serinv/algs/ddbtaf.py
+++ src/serinv/algs/ddbtaf.py
@@ -48,22 +48,18 @@ def ddbtaf(
 
     Returns
     -------
-    L_diagonal_blocks : ArrayLike
-        Diagonal blocks of the lower factor.
-    L_lower_diagonal_blocks : ArrayLike
-        Lower diagonal blocks of the lower factor.
-    L_arrow_bottom_blocks : ArrayLike
-        Bottom arrow blocks of the lower factor.
-    L_arrow_tip_block : ArrayLike
-        Tip arrow block of the lower factor.
-    U_diagonal_blocks : ArrayLike
-        Diagonal blocks of the upper factor.
-    U_upper_diagonal_blocks : ArrayLike
-        Upper diagonal blocks of the upper factor
-    U_arrow_right_blocks : ArrayLike
-        Right arrow blocks of the upper factor
-    U_arrow_tip_block : ArrayLike
-        Tip arrow block of the upper factor
+    LU_diagonal_blocks : ArrayLike
+        LU factors of the diagonal blocks.
+    LU_lower_diagonal_blocks : ArrayLike
+        LU factors of the lower diagonal blocks.
+    LU_upper_diagonal_blocks : ArrayLike
+        LU factors of the upper diagonal blocks.
+    LU_arrow_bottom_blocks : ArrayLike
+        LU factors of the bottom arrow blocks.
+    LU_arrow_right_blocks : ArrayLike
+        LU factors of the right arrow blocks.
+    LU_arrow_tip_block : ArrayLike
+        LU factors of the tip block of the arrowhead.
     """
 
     la = np_la
@@ -76,46 +72,47 @@ def ddbtaf(
 
     n_diag_blocks = A_diagonal_blocks.shape[0]
 
-    L_diagonal_blocks = xp.empty_like(A_diagonal_blocks)
-    L_lower_diagonal_blocks = xp.empty_like(A_lower_diagonal_blocks)
-    L_arrow_bottom_blocks = xp.empty_like(A_arrow_bottom_blocks)
-    L_arrow_tip_block = xp.empty_like(A_arrow_tip_block)
+    # LU aliases
+    LU_diagonal_blocks = A_diagonal_blocks
+    LU_lower_diagonal_blocks = A_lower_diagonal_blocks
+    LU_upper_diagonal_blocks = A_upper_diagonal_blocks
+    LU_arrow_bottom_blocks = A_arrow_bottom_blocks
+    LU_arrow_right_blocks = A_arrow_right_blocks
+    LU_arrow_tip_block = A_arrow_tip_block
 
-    U_diagonal_blocks = xp.empty_like(A_diagonal_blocks)
-    U_upper_diagonal_blocks = xp.empty_like(A_upper_diagonal_blocks)
-    U_arrow_right_blocks = xp.empty_like(A_arrow_right_blocks)
-    U_arrow_tip_block = xp.empty_like(A_arrow_tip_block)
+    # Pivots array
+    P_diag = xp.zeros((n_diag_blocks, A_diagonal_blocks.shape[1]), dtype=xp.int32)
+    P_tip = xp.zeros((A_arrow_tip_block.shape[1]), dtype=xp.int32)
 
     for i in range(0, n_diag_blocks - 1):
         # L_{i, i}, U_{i, i} = lu_dcmp(A_{i, i})
         (
-            L_diagonal_blocks[i, :, :],
-            U_diagonal_blocks[i, :, :],
-        ) = la.lu(
+            LU_diagonal_blocks[i, :, :],
+            P_diag[i, :],
+        ) = la.lu_factor(
             A_diagonal_blocks[i, :, :],
-            permute_l=True,
         )
 
         # Compute lower factors
         # L_{i+1, i} = A_{i+1, i} @ U{i, i}^{-1}
-        L_lower_diagonal_blocks[i, :, :] = (
+        LU_lower_diagonal_blocks[i, :, :] = (
             la.solve_triangular(
-                U_diagonal_blocks[i, :, :],
+                LU_diagonal_blocks[i, :, :],
                 A_lower_diagonal_blocks[i, :, :].conj().T,
                 lower=False,
-                trans="T" if L_diagonal_blocks.dtype.char == "f" else "C",
+                trans="T" if LU_diagonal_blocks.dtype.char == "f" else "C",
             )
             .conj()
             .T
         )
 
         # L_{ndb+1, i} = A_{ndb+1, i} @ U{i, i}^{-1}
-        L_arrow_bottom_blocks[i, :, :] = (
+        LU_arrow_bottom_blocks[i, :, :] = (
             la.solve_triangular(
-                U_diagonal_blocks[i, :, :],
+                LU_diagonal_blocks[i, :, :],
                 A_arrow_bottom_blocks[i, :, :].conj().T,
                 lower=False,
-                trans="T" if L_diagonal_blocks.dtype.char == "f" else "C",
+                trans="T" if LU_diagonal_blocks.dtype.char == "f" else "C",
             )
             .conj()
             .T
@@ -123,92 +120,97 @@ def ddbtaf(
 
         # Compute upper factors
         # U_{i, i+1} = L{i, i}^{-1} @ A_{i, i+1}
-        U_upper_diagonal_blocks[i, :, :] = la.solve_triangular(
-            L_diagonal_blocks[i, :, :],
+        LU_upper_diagonal_blocks[i, :, :] = la.solve_triangular(
+            LU_diagonal_blocks[i, :, :],
             A_upper_diagonal_blocks[i, :, :],
             lower=True,
+            unit_diagonal=True,
         )
 
         # U_{i, ndb+1} = L{i, i}^{-1} @ A_{i, ndb+1}
-        U_arrow_right_blocks[i, :, :] = la.solve_triangular(
-            L_diagonal_blocks[i, :, :],
+        LU_arrow_right_blocks[i, :, :] = la.solve_triangular(
+            LU_diagonal_blocks[i, :, :],
             A_arrow_right_blocks[i, :, :],
             lower=True,
+            unit_diagonal=True,
         )
 
         # Update next diagonal block
         # A_{i+1, i+1} = A_{i+1, i+1} - L_{i+1, i} @ U_{i, i+1}
         A_diagonal_blocks[i + 1, :, :] = (
             A_diagonal_blocks[i + 1, :, :]
-            - L_lower_diagonal_blocks[i, :, :] @ U_upper_diagonal_blocks[i, :, :]
+            - LU_lower_diagonal_blocks[i, :, :] @ LU_upper_diagonal_blocks[i, :, :]
         )
 
         # Update next upper/lower blocks of the arrowhead
         # A_{ndb+1, i+1} = A_{ndb+1, i+1} - L_{ndb+1, i} @ U_{i, i+1}
         A_arrow_bottom_blocks[i + 1, :, :] = (
             A_arrow_bottom_blocks[i + 1, :, :]
-            - L_arrow_bottom_blocks[i, :, :] @ U_upper_diagonal_blocks[i, :, :]
+            - LU_arrow_bottom_blocks[i, :, :] @ LU_upper_diagonal_blocks[i, :, :]
         )
 
         # A_{i+1, ndb+1} = A_{i+1, ndb+1} - L_{i+1, i} @ U_{i, ndb+1}
         A_arrow_right_blocks[i + 1, :, :] = (
             A_arrow_right_blocks[i + 1, :, :]
-            - L_lower_diagonal_blocks[i, :, :] @ U_arrow_right_blocks[i, :, :]
+            - LU_lower_diagonal_blocks[i, :, :] @ LU_arrow_right_blocks[i, :, :]
         )
 
         # Update the block at the tip of the arrowhead
         # A_{ndb+1, ndb+1} = A_{ndb+1, ndb+1} - L_{ndb+1, i} @ U_{i, ndb+1}
         A_arrow_tip_block[:, :] = (
             A_arrow_tip_block[:, :]
-            - L_arrow_bottom_blocks[i, :, :] @ U_arrow_right_blocks[i, :, :]
+            - LU_arrow_bottom_blocks[i, :, :] @ LU_arrow_right_blocks[i, :, :]
         )
 
     # L_{ndb, ndb}, U_{ndb, ndb} = lu_dcmp(A_{ndb, ndb})
     (
-        L_diagonal_blocks[-1, :, :],
-        U_diagonal_blocks[-1, :, :],
-    ) = la.lu(
+        LU_diagonal_blocks[-1, :, :],
+        P_diag[-1, :],
+    ) = la.lu_factor(
         A_diagonal_blocks[-1, :, :],
-        permute_l=True,
     )
 
     # L_{ndb+1, ndb} = A_{ndb+1, ndb} @ U_{ndb, ndb}^{-1}
-    L_arrow_bottom_blocks[-1, :, :] = (
+    LU_arrow_bottom_blocks[-1, :, :] = (
         la.solve_triangular(
-            U_diagonal_blocks[-1, :, :],
+            LU_diagonal_blocks[-1, :, :],
             A_arrow_bottom_blocks[-1, :, :].conj().T,
             lower=False,
-            trans="T" if L_diagonal_blocks.dtype.char == "f" else "C",
+            trans="T" if LU_diagonal_blocks.dtype.char == "f" else "C",
         )
         .conj()
         .T
     )
 
     # U_{ndb, ndb+1} = L_{ndb, ndb}^{-1} @ A_{ndb, ndb+1}
-    U_arrow_right_blocks[-1, :, :] = la.solve_triangular(
-        L_diagonal_blocks[-1, :, :],
+    LU_arrow_right_blocks[-1, :, :] = la.solve_triangular(
+        LU_diagonal_blocks[-1, :, :],
         A_arrow_right_blocks[-1, :, :],
         lower=True,
+        unit_diagonal=True,
     )
 
     # A_{ndb+1, ndb+1} = A_{ndb+1, ndb+1} - L_{ndb+1, ndb} @ U_{ndb, ndb+1}
     A_arrow_tip_block[:, :] = (
         A_arrow_tip_block[:, :]
-        - L_arrow_bottom_blocks[-1, :, :] @ U_arrow_right_blocks[-1, :, :]
+        - LU_arrow_bottom_blocks[-1, :, :] @ LU_arrow_right_blocks[-1, :, :]
     )
 
     # L_{ndb+1, ndb+1}, U_{ndb+1, ndb+1} = lu_dcmp(A_{ndb+1, ndb+1})
-    (L_arrow_tip_block[:, :], U_arrow_tip_block[:, :]) = la.lu(
-        A_arrow_tip_block[:, :], permute_l=True
+    (
+        LU_arrow_tip_block[:, :],
+        P_tip[:],
+    ) = la.lu_factor(
+        A_arrow_tip_block[:, :],
     )
 
     return (
-        L_diagonal_blocks,
-        L_lower_diagonal_blocks,
-        L_arrow_bottom_blocks,
-        L_arrow_tip_block,
-        U_diagonal_blocks,
-        U_upper_diagonal_blocks,
-        U_arrow_right_blocks,
-        U_arrow_tip_block,
+        LU_diagonal_blocks,
+        LU_lower_diagonal_blocks,
+        LU_upper_diagonal_blocks,
+        LU_arrow_bottom_blocks,
+        LU_arrow_right_blocks,
+        LU_arrow_tip_block,
+        P_diag,
+        P_tip,
     )
