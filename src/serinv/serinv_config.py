5e4317feb7bd3ee1290dd0ce650c0e7f8c47aac9
vincent-maillou <vmaillou@iis.ee.ethz.ch>
2024-11-20 14:13:03 +0100

    remove everything that is not properly finished or working yet.

--- src/serinv/serinv_config.py
+++
@@ -1,8 +0,0 @@
-# Copyright 2023-2024 ETH Zurich and the QuaTrEx authors. All rights reserved.
-
-from pydantic import BaseModel
-
-
-class SolverConfig(BaseModel):
-    device_streaming: bool = True
-    cuda_aware_mpi: bool = False
