aa5c893c3fde04cc120eb55dcca23c761746a32e
03szust <nafets111222da@gmail.com>
2025-04-24 15:32:28 +0000

    inserted ifs for termination

--- src/serinv/algs/pobtas.py
+++ src/serinv/algs/pobtas.py
@@ -313,8 +313,17 @@ def _pobtas_streaming(
 
     n_diag_blocks: int = L_diagonal_blocks.shape[0] # why?
     if n_diag_blocks > 1:
+        B_d[1].set(arr=B[1 * diag_blocksize : 2 * diag_blocksize], stream = h2d_stream)
+        h2d_B_events[1].record(stream=h2d_stream)
+
         L_lower_diagonal_blocks_d[0].set(arr=L_lower_diagonal_blocks[0], stream=h2d_stream)
         h2d_lower_diagonal_events[0].record(stream=h2d_stream)
+
+        L_diagonal_blocks_d[1].set(arr=L_diagonal_blocks[1], stream=h2d_stream)
+        h2d_diagonal_events[1].record(stream=h2d_stream)
+
+        L_lower_diagonal_blocks_d[1].set(arr=L_lower_diagonal_blocks[i + 1], stream=h2d_stream)
+        h2d_lower_diagonal_events[1].record(stream=h2d_stream)
 
 
     if trans == "N":
@@ -332,9 +341,10 @@ def _pobtas_streaming(
                 )
                 compute_current_B_events[i % 2].record(stream=compute_stream)
             
-            h2d_stream.wait_event(compute_current_B_events[i % 2])
-            L_diagonal_blocks_d[(i + 1) % 2].set(arr=L_diagonal_blocks[i + 1], stream=h2d_stream)
-            h2d_diagonal_events[i % 2].record(stream=h2d_stream)
+            if i + 2 < n_diag_blocks - 1:
+                h2d_stream.wait_event(compute_current_B_events[i % 2])
+                L_diagonal_blocks_d[(i + 2) % 2].set(arr=L_diagonal_blocks[i + 2], stream=h2d_stream)
+                h2d_diagonal_events[(i + 2) % 2].record(stream=h2d_stream)
 
             d2h_stream.wait_event(compute_next_B_events[i % 2])
             B_d[i % 2].get(
@@ -356,9 +366,10 @@ def _pobtas_streaming(
                 )
                 compute_next_B_events[i % 2].record(stream=compute_stream)
 
-            h2d_stream.wait_event(compute_next_B_events[i % 2])
-            L_lower_diagonal_blocks_d[(i + 1) % 2].set(arr=L_lower_diagonal_blocks[i + 1], stream=h2d_stream)
-            h2d_lower_diagonal_events[i % 2].record(stream=h2d_stream)
+            if i + 2 < n_diag_blocks - 1:
+                h2d_stream.wait_event(compute_next_B_events[i % 2])
+                L_lower_diagonal_blocks_d[(i + 2) % 2].set(arr=L_lower_diagonal_blocks[i + 2], stream=h2d_stream)
+                h2d_lower_diagonal_events[(i + 2) % 2].record(stream=h2d_stream)
                 
             with compute_stream:
                 # 3
@@ -371,12 +382,13 @@ def _pobtas_streaming(
                 )
                 compute_arrow_B_events[i % 2].record(stream=compute_stream)
 
-            h2d_stream.wait_event(compute_arrow_B_events[i % 2])
-            B_d[(i + 2) % 2].set(arr=B[(i + 2) * diag_blocksize : (i + 3) * diag_blocksize], stream = h2d_stream)
-            h2d_B_events[(i + 1) % 2].record(stream=h2d_stream)
+            if i + 2 < n_diag_blocks - 1:
+                h2d_stream.wait_event(compute_arrow_B_events[i % 2])
+                B_d[(i + 2) % 2].set(arr=B[(i + 2) * diag_blocksize : (i + 3) * diag_blocksize], stream = h2d_stream)
+                h2d_B_events[(i + 1) % 2].record(stream=h2d_stream)
 
-            L_lower_arrow_blocks_d[(i + 1) % 2].set(arr=L_lower_arrow_blocks[i + 1], stream=h2d_stream)
-            h2d_arrow_events[i % 2].record(stream=h2d_stream)
+                L_lower_arrow_blocks_d[(i + 1) % 2].set(arr=L_lower_arrow_blocks[i + 1], stream=h2d_stream)
+                h2d_arrow_events[(i + 1) % 2].record(stream=h2d_stream)
 
 
         if not partial:
