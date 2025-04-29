533af2a74560e93d586390a7c0055bb115c09e91
03szust <nafets111222da@gmail.com>
2025-04-29 23:21:53 +0000

    removed debug statements

--- src/serinv/algs/pobtas.py
+++ src/serinv/algs/pobtas.py
@@ -539,15 +539,13 @@ def _pobtas_streaming(
                 L_diagonal_blocks_d[(i - 1) % 2].set(arr=L_diagonal_blocks[i - 1], stream=h2d_stream)
                 L_lower_diagonal_blocks_d[(i - 1) % 2].set(arr=L_lower_diagonal_blocks[i - 1], stream=h2d_stream)
                 L_lower_arrow_blocks_d[(i - 1) % 2].set(arr=L_lower_arrow_blocks[i - 1], stream=h2d_stream)
+
             B_previous_d.set(arr=B[(i + 1) * diag_blocksize : (i + 2) * diag_blocksize], stream=h2d_stream)
 
             h2d_events[(i - 1) % 2].record(stream=h2d_stream)
             
             with compute_stream:
                 compute_stream.wait_event(h2d_events[i % 2])
-                print(i)
-                print(B)
-                print(B_previous_d)
                 B_d[i % 2] = cu_la.solve_triangular(
                     L_diagonal_blocks_d[i % 2],
                     B_d[i % 2]
