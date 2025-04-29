b394b91a4101c52185bed3b9af698541f13f45cb
03szust <nafets111222da@gmail.com>
2025-04-29 21:32:38 +0000

    changed debug

--- src/serinv/algs/pobtas.py
+++ src/serinv/algs/pobtas.py
@@ -484,7 +484,7 @@ def _pobtas_streaming(
                 arr=B[-arrow_blocksize - (2 * diag_blocksize) : -arrow_blocksize - diag_blocksize], 
                 stream=h2d_stream
             )
-            print(B)
+            print(L_diagonal_blocks)
             L_diagonal_blocks_d[n_diag_blocks % 2].set(arr=L_diagonal_blocks[-2], stream=h2d_stream)
             L_lower_arrow_blocks_d[n_diag_blocks % 2].set(arr=L_lower_arrow_blocks[-2], stream=h2d_stream)
             print(L_diagonal_blocks_d[n_diag_blocks % 2])
