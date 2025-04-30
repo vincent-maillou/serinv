f801076781c680256e8c4dd327ad3ffcba0e127a
03szust <nafets111222da@gmail.com>
2025-04-30 19:06:33 +0000

    insert debug

--- src/serinv/algs/pobtas.py
+++ src/serinv/algs/pobtas.py
@@ -543,6 +543,8 @@ def _pobtas_streaming(
             with compute_stream:
                 compute_stream.wait_event(h2d_events[(i - 1) % 2])
                 compute_stream.wait_event(d2h_events[(i - 1) % 2])
+                print(B_d)
+                print(B_previous_d)
                 B_previous_d[i % 2] = cu_la.solve_triangular(
                     L_diagonal_blocks_d[i % 2],
                     B_d[i % 2]
