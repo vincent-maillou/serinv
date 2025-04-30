dbe0abfc1732b2d34de0cbda4de6be60fcf57fba
03szust <nafets111222da@gmail.com>
2025-04-30 20:01:07 +0000

    another B_d print

--- src/serinv/algs/pobtas.py
+++ src/serinv/algs/pobtas.py
@@ -562,6 +562,7 @@ def _pobtas_streaming(
 
             d2h_stream.wait_event(compute_B_events[(i - 1) % 2])
             print("d2h")
+            print(B_previous_d)
             B_previous_d[(i + 1) % 2].get(out=B[(i + 1) * diag_blocksize : (i + 2) * diag_blocksize], stream=d2h_stream, blocking=False)
             d2h_events[i % 2].record(stream=d2h_stream)
 
