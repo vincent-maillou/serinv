8b6ba38163a8b061fe959c7afeb450cfd6df9499
03szust <nafets111222da@gmail.com>
2025-04-30 18:14:08 +0000

    changed previous B

--- src/serinv/algs/pobtas.py
+++ src/serinv/algs/pobtas.py
@@ -461,7 +461,9 @@ def _pobtas_streaming(
 
     elif trans == "T" or trans == "C":
         # Buffers
-        B_previous_d = cp.empty_like(B_shape)
+        B_previous_d = cp.empty(
+            (2, *B_shape.shape), dtype=B_shape.dtype
+        )
         del B_shape
         
         # Events
@@ -524,7 +526,7 @@ def _pobtas_streaming(
             L_lower_arrow_blocks_d[n_diag_blocks % 2].set(arr=L_lower_arrow_blocks[-2], stream=h2d_stream)
             L_lower_diagonal_blocks_d[n_diag_blocks % 2].set(arr=L_lower_diagonal_blocks[-1], stream=h2d_stream)
             h2d_stream.wait_event(previous_B_events[(n_diag_blocks - 1) % 2])
-            B_previous_d.set(arr=B[-arrow_blocksize - diag_blocksize : -arrow_blocksize], stream=h2d_stream)
+            B_previous_d[(n_diag_blocks - 1) % 2].set(arr=B[-arrow_blocksize - diag_blocksize : -arrow_blocksize], stream=h2d_stream)
             previous_B_events[(n_diag_blocks - 1) % 2].record(stream=d2h_stream)
             h2d_events[n_diag_blocks % 2].record(stream=h2d_stream)
 
@@ -535,7 +537,7 @@ def _pobtas_streaming(
 
             
             if i > 0:
-                h2d_stream.wait_event(previous_B_events[(i - 1 ) % 2])
+                h2d_stream.wait_event(compute_B_events[(i - 1 ) % 2])
                 B_d[(i - 1) % 2].set(arr=B[(i - 1) * diag_blocksize : i * diag_blocksize], stream=h2d_stream)
                 L_diagonal_blocks_d[(i - 1) % 2].set(arr=L_diagonal_blocks[i - 1], stream=h2d_stream)
                 L_lower_diagonal_blocks_d[(i - 1) % 2].set(arr=L_lower_diagonal_blocks[i - 1], stream=h2d_stream)
@@ -544,14 +546,11 @@ def _pobtas_streaming(
             
             with compute_stream:
                 compute_stream.wait_event(h2d_events[i % 2])
-                print(B)
-                print(B_d)
-                print(B_previous_d)
-                B_d[i % 2] = cu_la.solve_triangular(
+                B_previous_d[i % 2] = cu_la.solve_triangular(
                     L_diagonal_blocks_d[i % 2],
                     B_d[i % 2]
                     - L_lower_diagonal_blocks_d[i % 2].conj().T
-                    @ B_previous_d
+                    @ B_previous_d[(i - 1) % 2]
                     - L_lower_arrow_blocks_d[i % 2].conj().T @ B_arrow_tip_d,
                     lower=True,
                     trans="C",
@@ -559,19 +558,11 @@ def _pobtas_streaming(
 
                 compute_B_events[i % 2].record(compute_stream)
 
-            # d2h_stream.wait_event(previous_B_events[(i - 1) % 2])
-            d2h_stream.wait_event(h2d_events[i % 2])
-            B_previous_d.get(out=B[(i + 1) * diag_blocksize : (i + 2) * diag_blocksize], stream=d2h_stream, blocking=False)
+            d2h_stream.wait_event(compute_B_events[(i - 1) % 2])
+            B_previous_d[(i - 1) % 2].get(out=B[(i + 1) * diag_blocksize : (i + 2) * diag_blocksize], stream=d2h_stream, blocking=False)
             d2h_events[i % 2].record(stream=d2h_stream)
 
-            with compute_stream:
-                compute_stream.wait_event(d2h_events[i % 2])
-                print(B_previous_d)
-                B_previous_d = B_d[i % 2]
-                print(B_previous_d)
-                previous_B_events[i % 2].record(stream=compute_stream)
-
-        B_d[0].get(out=B[:diag_blocksize], stream=d2h_stream, blocking=False)
+        B_previous_d[0].get(out=B[:diag_blocksize], stream=d2h_stream, blocking=False)
             
     else:
         raise ValueError(f"Invalid transpose argument: {trans}.")
