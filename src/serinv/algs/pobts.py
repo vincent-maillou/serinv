ae7872847c6ebd9fcd6f4304fe07f71817b94b15
03szust <nafets111222da@gmail.com>
2025-05-01 15:19:48 +0000

    removed unnecessary events

--- src/serinv/algs/pobts.py
+++ src/serinv/algs/pobts.py
@@ -246,7 +246,6 @@ def _pobts_streaming(
             )
             L_diagonal_blocks_d[1].set(arr=L_diagonal_blocks[1], stream=h2d_stream)
             L_lower_diagonal_blocks_d[1].set(arr=L_lower_diagonal_blocks[0], stream=h2d_stream)
-            h2d_stream.wait_event(previous_B_events[0])
             B_previous_d[0].set(arr=B[:diag_blocksize], stream=h2d_stream)
             h2d_events[0].record(stream=h2d_stream)
 
@@ -315,7 +314,6 @@ def _pobts_streaming(
             )
             L_diagonal_blocks_d[n_diag_blocks % 2].set(arr=L_diagonal_blocks[-2], stream=h2d_stream)
             L_lower_diagonal_blocks_d[n_diag_blocks % 2].set(arr=L_lower_diagonal_blocks[-1], stream=h2d_stream)
-            h2d_stream.wait_event(previous_B_events[(n_diag_blocks - 1) % 2])
             B_previous_d[(n_diag_blocks - 1) % 2].set(arr=B[-diag_blocksize:], stream=h2d_stream)
             h2d_events[(n_diag_blocks - 1) % 2].record(stream=h2d_stream)
 
