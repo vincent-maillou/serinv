8bf1908c9a95986bd34d2b35c878da629fcef7f4
03szust <nafets111222da@gmail.com>
2025-05-01 16:01:14 +0000

    attempt to reduce streaming

--- src/serinv/algs/pobts.py
+++ src/serinv/algs/pobts.py
@@ -302,9 +302,9 @@ def _pobts_streaming(
 
             compute_B_events[(n_diag_blocks - 1) % 2].record(stream=compute_stream)
 
-        d2h_stream.wait_event(compute_B_events[(n_diag_blocks - 1) % 2])
+        #d2h_stream.wait_event(compute_B_events[(n_diag_blocks - 1) % 2])
 
-        B_previous_d[(n_diag_blocks - 1) % 2].get(out=B[-diag_blocksize:], stream=d2h_stream, blocking=False)
+        #B_previous_d[(n_diag_blocks - 1) % 2].get(out=B[-diag_blocksize:], stream=d2h_stream, blocking=False)
 
         d2h_events[(n_diag_blocks - 1) % 2].record(stream=d2h_stream)
 
@@ -352,7 +352,7 @@ def _pobts_streaming(
             
             d2h_events[i % 2].record(stream=d2h_stream)
 
-        if n_diag_blocks > 1:
+        if n_diag_blocks > 0:
             d2h_stream.wait_event(compute_B_events[0])
             
             B_previous_d[0].get(out=B[:diag_blocksize], stream=d2h_stream, blocking=False)
