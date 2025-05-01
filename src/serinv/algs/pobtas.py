eb67c786894480935a55f8bdeede6a02a86acd2a
03szust <nafets111222da@gmail.com>
2025-05-01 15:35:57 +0000

    removed unnecessary events

--- src/serinv/algs/pobtas.py
+++ src/serinv/algs/pobtas.py
@@ -468,7 +468,6 @@ def _pobtas_streaming(
         
         # Events
         compute_B_events = [cp.cuda.Event(), cp.cuda.Event()]
-        previous_B_events = [cp.cuda.Event(), cp.cuda.Event()]
         h2d_events = [cp.cuda.Event(), cp.cuda.Event()]
         d2h_events = [cp.cuda.Event(), cp.cuda.Event()]
         
@@ -525,7 +524,6 @@ def _pobtas_streaming(
             L_diagonal_blocks_d[n_diag_blocks % 2].set(arr=L_diagonal_blocks[-2], stream=h2d_stream)
             L_lower_arrow_blocks_d[n_diag_blocks % 2].set(arr=L_lower_arrow_blocks[-2], stream=h2d_stream)
             L_lower_diagonal_blocks_d[n_diag_blocks % 2].set(arr=L_lower_diagonal_blocks[-1], stream=h2d_stream)
-            h2d_stream.wait_event(previous_B_events[(n_diag_blocks - 1) % 2])
             B_previous_d[(n_diag_blocks - 1) % 2].set(arr=B[-arrow_blocksize - diag_blocksize : -arrow_blocksize], stream=h2d_stream)
             h2d_events[(n_diag_blocks - 1) % 2].record(stream=h2d_stream)
 
