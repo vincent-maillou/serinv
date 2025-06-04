bc1d67fa91e12d1c15dcc4f7e891610ec9b8be54
03szust <nafets111222da@gmail.com>
2025-06-04 08:52:47 +0000

    removed transpose

--- src/serinv/utils/trsm.py
+++ src/serinv/utils/trsm.py
@@ -324,7 +324,7 @@ def _solve_triangular(a1, b1, trans=0, lower=False, unit_diagonal=False,
                         trans_a=trans, diag=unit_diagonal, side=side)
     else:
         # transposed system is solved since trtrs expects Fortran ordering
-        x = trsm(alpha, a1.T, b1, overwrite_b=overwrite_b, lower=not lower,
+        x = trsm(alpha, a1, b1, overwrite_b=overwrite_b, lower=not lower,
                         trans_a=not trans, diag=unit_diagonal, side=side)
 
     return x
