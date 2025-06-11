1436b7f149d4d6c085a650ae2f45ba452099d3ea
03szust <nafets111222da@gmail.com>
2025-06-11 09:33:58 +0000

    fixed typo

--- src/serinv/block_primitive/__init__.py
+++ src/serinv/block_primitive/__init__.py
@@ -1,6 +1,6 @@
 from serinv.block_primitive.gemm import gemm
 from serinv.block_primitive.trsm import trsm
-from serinv.block_primitive.trsm import syherk
+from serinv.block_primitive.syherk import syherk
 
 __all__ = [
     "gemm",
