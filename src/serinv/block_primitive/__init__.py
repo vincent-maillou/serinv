5a8f26828630c7541de950b4d2a5d22d6eb79ecf
03szust <nafets111222da@gmail.com>
2025-06-11 12:28:33 +0000

    attempt to fix import problem

--- src/serinv/block_primitive/__init__.py
+++ src/serinv/block_primitive/__init__.py
@@ -1,9 +1,11 @@
 from serinv.block_primitive.gemm import gemm
 from serinv.block_primitive.trsm import trsm
 from serinv.block_primitive.syherk import syherk
+import cupyfix_backends
 
 __all__ = [
     "gemm",
     "trsm",
-    "syherk"
+    "syherk",
+    cupyfix_backends
 ]
