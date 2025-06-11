2bf27a49f2615a2cb293e93c983fdcf7b3048dc9
03szust <nafets111222da@gmail.com>
2025-06-11 09:43:19 +0000

    attempt for using syherk

--- src/serinv/algs/pobtaf.py
+++ src/serinv/algs/pobtaf.py
@@ -7,7 +7,7 @@ from serinv import (
     _get_cholesky,
 )
 
-from serinv.block_primitive import trsm, gemm
+from serinv.block_primitive import trsm, gemm, syherk
 
 def pobtaf(
     A_diagonal_blocks: ArrayLike,
@@ -170,7 +170,6 @@ def _pobtaf(
             )
         )
         print(A_arrow_tip_block[:, :])
-        raise ValueError("TEST")
 
     if factorize_last_block:
         # L_{ndb, ndb} = chol(A_{ndb, ndb})
@@ -188,14 +187,21 @@ def _pobtaf(
         )
 
         # A_{ndb+1, ndb+1} = A_{ndb+1, ndb+1} - L_{ndb+1, ndb} @ L_{ndb+1, ndb}^{T}
+    #    A_arrow_tip_block[:, :] = (
+    #        gemm(
+    #            L_lower_arrow_blocks[-1, :, :],
+    #            L_lower_arrow_blocks[-1, :, :],
+    #            A_arrow_tip_block[:, :],
+    #            trans_b='C', alpha=-1.0, beta=1.0
+    #        )
+    #    )
+
         A_arrow_tip_block[:, :] = (
-            gemm(
-                L_lower_arrow_blocks[-1, :, :],
+            syherk(
                 L_lower_arrow_blocks[-1, :, :],
                 A_arrow_tip_block[:, :],
-                trans_b='C', alpha=-1.0, beta=1.0
+                alpha=-1.0, beta=1.0, lower=True
             )
-        )
 
         # L_{ndb+1, ndb+1} = chol(A_{ndb+1, ndb+1})
         L_arrow_tip_block[:, :] = cholesky(A_arrow_tip_block[:, :])
