bd0e04316bb4226d25db1cd2d3c62b692dae50fb
03szust <nafets111222da@gmail.com>
2025-06-11 12:30:32 +0000

    further debugging

--- src/serinv/block_primitive/syherk.py
+++ src/serinv/block_primitive/syherk.py
@@ -1,7 +1,7 @@
 from serinv import _get_module_from_array
 
 #import trymod
-from cupyfix_backends.cuda.libs import cublas as cublasfix
+from block_primitive.cupyfix_backends.cuda.libs import cublas as cublasfix
 
 import numpy as np
 from numpy.linalg import matmul
