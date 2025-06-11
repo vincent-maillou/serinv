35c5c07ea021abc83fb133a3d3bb97c95b23e432
03szust <nafets111222da@gmail.com>
2025-06-11 22:34:58 +0000

    changed loaction of.h files

--- setup.py
+++ setup.py
@@ -3,17 +3,18 @@ from Cython.Build import cythonize
 
 ext = Extension(
     name="cupyfix_backends.cuda.libs.cublas",
-    sources=["src/serinv/cupyfix_backends/cuda/libs/cublas.pxd",
-             "src/serinv/cupyfix_backends/cuda/libs/cublas.pyx",
-             "src/serinv/cupyfix_backends/cuda/cupy_cublas.h",
-             "src/serinv/cupyfix_backends/cuda/hip/cupy_cuComplex.h",
-             "src/serinv/cupyfix_backends/cuda/hip/cupy_hip_common.h",
-             "src/serinv/cupyfix_backends/cuda/hip/cupy_hipblas.h",
-             "src/serinv/cupyfix_backends/cuda/stub/cupy_cublas.h",
-             "src/serinv/cupyfix_backends/cuda/stub/cupy_cuComplex.h",
-             "src/serinv/cupyfix_backends/cuda/cupy_blas.h"
-             "src/serinv/cupyfix_backends/cuda/cupy_complex.h"],
-             include_dirs=["cupyfix_backends"],
+    sources=[
+             "src/serinv/cupyfix_backends/cuda/libs/cublas.pyx"],
+    include_dirs=["src/serinv/cupyfix_backends/cuda/libs/cublas.pxd",
+                "src/serinv/cupyfix_backends/cuda/cupy_cublas.h",
+                "src/serinv/cupyfix_backends/cuda/hip/cupy_cuComplex.h",
+                "src/serinv/cupyfix_backends/cuda/hip/cupy_hip_common.h",
+                "src/serinv/cupyfix_backends/cuda/hip/cupy_hipblas.h",
+                "src/serinv/cupyfix_backends/cuda/stub/cupy_cublas.h",
+                "src/serinv/cupyfix_backends/cuda/stub/cupy_cuComplex.h",
+                "src/serinv/cupyfix_backends/cuda/cupy_blas.h",
+                "src/serinv/cupyfix_backends/cuda/cupy_complex.h"
+                ],
 )
 
 setup(
