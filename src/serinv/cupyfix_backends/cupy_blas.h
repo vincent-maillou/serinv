5398b4a59cd1a8683bdf93da728ae0cc53c92a57
03szust <nafets111222da@gmail.com>
2025-06-11 23:29:46 +0000

    changed include to cuda

--- src/serinv/cupyfix_backends/cupy_blas.h
+++ src/serinv/cupyfix_backends/cupy_blas.h
@@ -1,7 +1,7 @@
 #ifndef INCLUDE_GUARD_CUPY_CUBLAS_H
 #define INCLUDE_GUARD_CUPY_CUBLAS_H
 
-#include "hip/cupy_hipblas.h"
+#include "cuda/cupy_cublas.h"
 
 
 #endif // #ifndef INCLUDE_GUARD_CUPY_CUBLAS_H
