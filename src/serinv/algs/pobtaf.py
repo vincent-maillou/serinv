15deadfe535aedd24f2b028b0008b7bd082b9315
03szust <nafets111222da@gmail.com>
2025-06-12 10:27:30 +0000

    sanity 1

--- src/serinv/algs/pobtaf.py
+++ src/serinv/algs/pobtaf.py
@@ -199,15 +199,16 @@ def _pobtaf(
                 L_lower_arrow_blocks[-1, :, :],
                trans_b='C', alpha=1.0))
 
-        print(A_arrow_tip_block[:, :]-syherk(L_lower_arrow_blocks[-1, :, :],lower=True))
+        A_arrow_tip_block[:, :]-=syherk(L_lower_arrow_blocks[-1, :, :],lower=True)
 
-        A_arrow_tip_block[:, :] = (
-            syherk(
-                L_lower_arrow_blocks[-1, :, :],
-                A_arrow_tip_block[:, :],
-                alpha=-1.0, beta=1.0, lower=True
-            )
-        )
+        #A_arrow_tip_block[:, :] = (
+        #    syherk(
+        #        L_lower_arrow_blocks[-1, :, :],
+        #        A_arrow_tip_block[:, :],
+        #        alpha=-1.0, beta=1.0, lower=True
+        #    )
+        #)
+
         print(A_arrow_tip_block[:, :])
 
         # L_{ndb+1, ndb+1} = chol(A_{ndb+1, ndb+1})
