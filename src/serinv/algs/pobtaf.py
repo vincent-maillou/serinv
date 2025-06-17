3a1ad6e6a3c46378bf9428525608fe0957630285
03szust <nafets111222da@gmail.com>
2025-06-17 08:36:09 +0000

    print added to check for error

--- src/serinv/algs/pobtaf.py
+++ src/serinv/algs/pobtaf.py
@@ -770,9 +770,10 @@ def _pobtaf_permuted_streaming(
                 syherk(
                     L_lower_diagonal_blocks_d[i % 2, :, :],
                     A_diagonal_blocks_d[(i + 1) % 2, :, :],
-                    alpha=-1.0, beta=1.0, lower=True, cu_chol=False
+                    alpha=-1.0, beta=1.0, lower=True, cu_chol=True
                 )
             )
+            print(A_diagonal_blocks_d[(i + 1) % 2, :, :])
 
             # A_{ndb+1, i+1} = A_{ndb+1, i+1} - L_{ndb+1, i} @ L_{i+1, i}.conj().T
             A_lower_arrow_blocks_d[(i + 1) % 2, :, :] = (
