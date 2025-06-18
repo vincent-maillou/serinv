c90226b2e9f74670b7adaf1eab2e54447545cefd
03szust <nafets111222da@gmail.com>
2025-06-18 09:41:37 +0000

    reverted switch

--- src/serinv/algs/pobtas.py
+++ src/serinv/algs/pobtas.py
@@ -102,9 +102,8 @@ def _pobtas(
             #)
             B[(i + 1) * diag_blocksize : (i + 2) * diag_blocksize] = (
                 gemm(
-                    
-                    B[i * diag_blocksize : (i + 1) * diag_blocksize],
                     L_lower_diagonal_blocks[i],
+                    B[i * diag_blocksize : (i + 1) * diag_blocksize],
                     B[(i + 1) * diag_blocksize : (i + 2) * diag_blocksize],
                     alpha=-1.0, beta=1.0
                 )
