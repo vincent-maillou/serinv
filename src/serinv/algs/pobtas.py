118745e303a7f7fa98191a75bdd2af77112eb1f9
03szust <nafets111222da@gmail.com>
2025-06-18 08:22:02 +0000

    reset streaming code to original

--- src/serinv/algs/pobtas.py
+++ src/serinv/algs/pobtas.py
@@ -7,7 +7,6 @@ from serinv import (
     _get_module_from_str,
 )
 
-from serinv.block_primitive import trsm, gemm, syherk
 
 def pobtas(
     L_diagonal_blocks: ArrayLike,
@@ -88,14 +87,11 @@ def _pobtas(
     if trans == "N":
         # ----- Forward substitution -----
         for i in range(0, n_diag_blocks - 1):
-            B[i * diag_blocksize : (i + 1) * diag_blocksize] = (
-                trsm(
-                    L_diagonal_blocks[i],
-                    B[i * diag_blocksize : (i + 1) * diag_blocksize],
-                    lower=True
-                )
+            B[i * diag_blocksize : (i + 1) * diag_blocksize] = la.solve_triangular(
+                L_diagonal_blocks[i],
+                B[i * diag_blocksize : (i + 1) * diag_blocksize],
+                lower=True
             )
-            
 
             B[(i + 1) * diag_blocksize : (i + 2) * diag_blocksize] -= (
                 L_lower_diagonal_blocks[i]
