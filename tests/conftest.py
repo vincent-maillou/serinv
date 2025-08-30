8c595eb2cd7ecb587d934018dcde4afaa4ef7093
vincent-maillou <vmaillou@iis.ee.ethz.ch>
2025-08-30 16:20:10 +0200

    add tests runscripts for cluster env

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,6 +1,7 @@
 # Copyright 2023-2025 ETH Zurich. All rights reserved.
 # Global pytest fixtures for the Serinv tests.
 
+import os
 import pytest
 
 from serinv import backend_flags
@@ -26,6 +27,14 @@ DIAGONAL_BLOCKSIZE = [
     pytest.param(3, id="diagonal_blocksize=3"),
 ]
 
+if os.environ.get("LARGE_TESTS", "0") == "1":
+    DIAGONAL_BLOCKSIZE.extend(
+        [
+            pytest.param(512, id="diagonal_blocksize=512"),
+            pytest.param(1024, id="diagonal_blocksize=1024"),
+            pytest.param(2048, id="diagonal_blocksize=2048"),
+        ]
+    )
 
 @pytest.fixture(params=ARRAY_TYPE, autouse=True)
 def array_type(request: pytest.FixtureRequest) -> str:
---
+++ tests/run_tests_sequential.sh
@@ -0,0 +1,33 @@
+#!/bin/bash -l
+#SBATCH --job-name="tests_serinv"
+#SBATCH --output=%x.%j.out
+#SBATCH --error=%x.%j.err
+#SBATCH --account=g186
+### SBATCH --account=sm96
+#SBATCH --time=00:10:00
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=1
+#SBATCH --cpus-per-task=64
+#SBATCH --gpus-per-task=1
+#SBATCH --partition=debug
+#SBATCH --constraint=gpu
+#SBATCH --hint=nomultithread
+#SBATCH --uenv=prgenv-gnu/24.11:v1
+#SBATCH --view=modules
+
+set -e -u
+
+export OMP_NUM_THREADS=$SLURM_CPUS_PER_TASK
+export MPICH_GPU_SUPPORT_ENABLED=1
+
+export NCCL_NET='AWS Libfabric'
+export CUDA_VISIBLE_DEVICES=$SLURM_LOCALID
+
+source ~/load_modules.sh
+conda activate allin
+
+export MPI_CUDA_AWARE=1
+export LARGE_TESTS=1
+
+# --- Microbenchmark ---
+srun pytest ~/repositories/serinv/
